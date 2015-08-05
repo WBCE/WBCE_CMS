8decb62c30479316b0dcd4170f77aaa76d7e54c4
cwsoft <info@cwsoft.de>
2015-08-06 00:19:57 +0200

    Added bash script to create CHANGELOG.md on the fly
    - changelog needed for GNU GPL compatibility
    - most recent CHANGELOG is always available at WBCE GitHub repository

---
+++ create_changelog.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#####################################################################
+# WebsiteBaker Community Edition (WBCE)
+# More Baking. Less Struggling.
+# Visit http://wbce.org to learn more and to join the community.
+#
+# Usage: Call script inside your local WBCE Git root folder to dump
+# the commit messages of the WBCE master branch to CHANGELOG.md.
+#
+# @copyright WBCE Project (2015-)
+# @license GNU GPL2 (or any later version)
+#####################################################################
+# Define some variables
+WBCE_URL=https://github.com/WBCE/WebsiteBaker_CommunityEdition
+OUTPUT_FILE=CHANGELOG.md
+
+# Output general header to changelog
+cat > $OUTPUT_FILE << EOT
+# Changelog WebsiteBaker CE
+This CHANGELOG was automatically created from a local WBCE Git repository.
+The changelog may therefore not be correct or up-to date.
+Please visit the [WBCE Github]($WBCE_URL/commits) repository for the most up to-date version.
+
+-------------------- AUTO GENERATED GIT COMMIT HISTORY --------------------"
+EOT
+
+# Output Git commit history using pretty-format
+git log --oneline --pretty=format:"%ad [%h]($WBCE_URL/commit/%H): %an%n%s%n%w(0,3,3)%b" --date=short >> $OUTPUT_FILE
+
+echo
+echo ">> Created $OUTPUT_FILE in actual working folder"
