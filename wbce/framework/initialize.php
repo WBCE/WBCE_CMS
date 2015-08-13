a15303c3fc942ce53ae8dc70a12db14692dedbcd
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2015-08-13 18:51:54 +0200

    fix for issue #24

--- wbce/framework/initialize.php
+++ wbce/framework/initialize.php
@@ -83,6 +83,7 @@ if (file_exists(WB_PATH.'/framework/class.database.php')) {
     $sTmp = dirname(dirname(__FILE__)).'/include/Sensio/Twig/lib/Twig/Autoloader.php';
     if (!class_exists('Twig_Autoloader') && is_readable($sTmp)){ 
         include($sTmp); 
+    }
 
 	// register PHPMailer autoloader ---
     $sTmp = dirname(dirname(__FILE__)).'/include/phpmailer/PHPMailerAutoload.php';
