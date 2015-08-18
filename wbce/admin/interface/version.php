51936d940ea8c804d085eafe7a76758e1a6aeae0
cwsoft <info@cwsoft.de>
2015-08-19 00:00:27 +0200

    Set version to v1.0.0-Beta3

--- wbce/admin/interface/version.php
+++ wbce/admin/interface/version.php
@@ -10,18 +10,29 @@
  * @license GNU GPL2 (or any later version)
  */
 
-if(!defined('WB_URL')) {
-	header('Location: ../index.php');
-	exit(0);
+if (!defined('WB_URL')) {
+    header('Location: ../index.php');
+    exit(0);
 }
 
 // set WBCE version and release tag
-if(!defined('WBCE_VERSION')) define('WBCE_VERSION', '1.0.0');
-if(!defined('WBCE_TAG')) define('WBCE_TAG', 'WBCE1Beta2');
+if (!defined('WBCE_VERSION')) {
+    define('WBCE_VERSION', '1.0.0');
+}
+
+if (!defined('WBCE_TAG')) {
+    define('WBCE_TAG', 'v1.0.0-Beta3');
+}
 
 // Legacy: WB-classic
-if(!defined('VERSION')) define('VERSION', '2.8.3');
-if(!defined('REVISION')) define('REVISION', '1641');
-if(!defined('SP')) define('SP', 'SP4');
+if (!defined('VERSION')) {
+    define('VERSION', '2.8.3');
+}
 
+if (!defined('REVISION')) {
+    define('REVISION', '1641');
+}
 
+if (!defined('SP')) {
+    define('SP', 'SP4');
+}
