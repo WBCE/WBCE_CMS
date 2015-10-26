697a5b4272ca84375038fb600d8e03276845e6ac
NorHei <norbert@heimsath.org>
2015-10-26 18:08:52 +0100

    frontend.functions.php Function moveCssToHead is obsolete/not in use!
    has been removed as this is done by an outputfilter.
    
    Thx to Kant

--- wbce/framework/frontend.functions.php
+++ wbce/framework/frontend.functions.php
@@ -656,24 +656,4 @@ if (!function_exists('register_frontend_modfiles')) {
     }
 }
 
-function moveCssToHead($content)
-{
-    // move css definitions into head section
-    $pattern1 = '/(?:<body.*?)(<link[^>]*?\"text\/css\".*?\/>)/si';
-    $pattern2 = '/(?:<body.*?)(<style[^>]*?\"text\/css\"[^>]*?>.*?<\/style>)/si';
-    while (preg_match($pattern1, $content, $matches) == 1) {
-        // loop through all linked CSS
-        $insert = $matches[1];
-        $content = str_replace($insert, '', $content);
-        $insert = "\n" . $insert . "\n</head>\n<body";
-        $content = preg_replace('/<\/head>.*?<body/si', $insert, $content);
-    }
-    while (preg_match($pattern2, $content, $matches) == 1) {
-        // loop through all inline CSS
-        $insert = $matches[1];
-        $content = str_replace($insert, '', $content);
-        $insert = "\n" . $insert . "\n</head>\n<body";
-        $content = preg_replace('/<\/head>.*?<body/si', $insert, $content);
-    }
-    return $content;
-}
+
