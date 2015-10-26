9004e66bffa248658713ec682d08fad173d48538
NorHei <norbert@heimsath.org>
2015-10-26 08:07:14 +0100

    MoveCSStoHead was running into an endless loop on some occasions.
    Calling phpinfo() from a code section for example.
    
    This is the fix from the last public svn of WB.
    Thx to Kant

--- wbce/modules/output_filter/filters/filterCssToHead.php
+++ wbce/modules/output_filter/filters/filterCssToHead.php
@@ -4,23 +4,23 @@
  * @param string $content
  * @return string
  */
-	function doFilterCssToHead($content) {
-		// move css definitions into head section
-		$pattern1 = '/(?:<body.*?)(<link[^>]*?\"text\/css\".*?\/>)/si';
-		$pattern2 = '/(?:<body.*?)(<style[^>]*?\"text\/css\"[^>]*?>.*?<\/style>)/si';
-		while(preg_match($pattern1, $content, $matches)==1) {
-		// loop through all linked CSS
-			$insert = $matches[1];
-			$content = str_replace($insert, '', $content);
-			$insert = "\n".$insert."\n</head>\n<body";
-			$content = preg_replace('/<\/head>.*?<body/si', $insert, $content);
-		}
-		while(preg_match($pattern2, $content, $matches)==1) {
-		// loop through all inline CSS
-			$insert = $matches[1];
-			$content = str_replace($insert, '', $content);
-			$insert = "\n".$insert."\n</head>\n<body";
-			$content = preg_replace('/<\/head>.*?<body/si', $insert, $content);
-		}
-		return $content;
+function doFilterCssToHead($sContent) {
+	// move css definitions into head section
+	$sPattern1 = '/(?:<body.*?)(<link[^>]*?\"text\/css\".*?\/>)/si';
+	$sPattern2 = '/(?:<body.*?)(<style[^>]*?\"text\/css\"[^>]*?>.*?<\/style>)/si';
+	$aInsert = array();
+	while(preg_match($sPattern1, $sContent, $aMatches)) {
+		$aInsert[] = $aMatches[1];
+		$sContent = str_replace($aMatches[1], '', $sContent); 
 	}
+	while(preg_match($sPattern2, $sContent, $aMatches)) {
+		$aInsert[] = $aMatches[1];
+		$sContent = str_replace($aMatches[1], '', $sContent);
+	}
+	$aInsert = array_unique($aInsert);
+	if(sizeof($aInsert) > 0) {
+		$sInsert = "\n".implode("\n", $aInsert)."\n</head>\n<body";
+		$sContent = preg_replace('/<\/head>.*?<body/si', $sInsert, $sContent, 1);
+	}
+	return $sContent;
+}
