c1c8f95ec1e9f2da493cc2c9549b6e32584956ba
NorHei <norbert@heimsath.org>
2015-10-27 13:14:13 +0100

    Some security fixes on short URL
    short.php used to inject REQUEST_URI int SCRIPT_NAME and PHP_SELF.
    
    Especially SCRIPT_NAME is normally considered save for self refering forms.
    So its  not a good Idea to inject the complete unsave REQUEST_URI into it.
    
    Now we filter REQUEST_URI to provide some sane and save values for
    shortened urls.

--- wbce/short.php
+++ wbce/short.php
@@ -1,24 +1,5 @@
 <?php
-/*
 
-Website Baker Project <http://www.websitebaker.org/>
-Copyright (C) 2004-2009, Ryan Djurovich
-
-Website Baker is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Website Baker is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Website Baker; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- */
 /*
 Short.php & .htaccess example & Dropletcode
 Version 3.0 - June 19, 2013
@@ -31,8 +12,12 @@ $_ext = ".php";
 define('ERROR_PAGE', '/'); //Change this to point to your existing 404 page.
 
 if (isset($_GET['_wb'])) {
-    $_SERVER['PHP_SELF'] = $_SERVER['REQUEST_URI'];
-    $_SERVER['SCRIPT_NAME'] = $_SERVER['REQUEST_URI'];
+    $parsed=parse_url($_SERVER['REQUEST_URI']);
+    $pathed=pathinfo($parsed["path"]);
+    $scriptName=preg_replace("/(.php|.html|.htm).+$/u","$1",$parsed["path"]);
+    
+$_SERVER['PHP_SELF'] = $scriptName;
+    $_SERVER['SCRIPT_NAME'] = $scriptName;
     $page = trim($_GET['_wb'], '/');
     $fullpag = dirname(__FILE__) . $_pages . '/' . $page . $_ext;
     if (file_exists($fullpag)) {
@@ -52,31 +37,3 @@ if (isset($_GET['_wb'])) {
     header('Location: ' . ERROR_PAGE);
 }
 
-/* droplet code
-global $wb;
-$wb->preprocess( $wb_page_data);
-$linkstart = WB_URL.PAGES_DIRECTORY;
-$linkend = PAGE_EXTENSION;
-$nwlinkstart = WB_URL;
-$nwlinkend = '/';
-
-preg_match_all('~'.$linkstart.'(.*?)\\'.$linkend.'~', $wb_page_data, $links);
-foreach ($links[1] as $link) {
-$wb_page_data = str_replace($linkstart.$link.$linkend, $nwlinkstart.$link.$nwlinkend, $wb_page_data);
-}
-return true;
--- END droplet code */
-
-/* .htaccess
-RewriteEngine On
-# If called directly - redirect to short url version
-RewriteCond %{REQUEST_URI} !/pages/intro.php
-RewriteCond %{REQUEST_URI} /pages
-RewriteRule ^pages/(.*).php$ /$1/ [R=301,L]
-
-# Send the request to the wb.php for processing
-RewriteCond %{REQUEST_URI} !^/(pages|admin|framework|include|languages|media|account|search|temp|templates/.*)$
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteRule ^([\/\sa-zA-Z0-9._-]+)$ /wb.php?_wb=$1 [QSA,L]
--- END .htaccess */
