5efd6b528a2b2c46cd7aa8cc1e5e4610678e6ed9
NorHei <norbert@heimsath.org>
2015-11-23 17:40:37 +0100

    Wrong style align:... schould be text-align:...in module functions

--- wbce/framework/module.functions.php
+++ wbce/framework/module.functions.php
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 This file contains routines to edit the optional module files: frontend.css and backend.css
 Mechanism was introduced with WB 2.7 to provide a global solution for all modules
 To use this function, include this file from your module (e.g. from modify.php)
-Then simply call the function edit_css('your_module_directory') - that�s it
+Then simply call the function edit_css('your_module_directory') - that's it
 NOTE: Some functions were added for module developers to make the creation of own module easier
  */
 
@@ -94,7 +94,7 @@ if (!function_exists('edit_module_css')) {
             $edit_css_caption = (isset($GLOBALS['TEXT']['CAP_EDIT_CSS'])) ? $GLOBALS['TEXT']['CAP_EDIT_CSS'] : 'Edit CSS';
             ?>
 			<form name="edit_module_file" action="<?php echo WB_URL . '/modules/edit_module_files.php?page_id=' . $page_id;?>"
-				method="post" style="margin: 0; align:right;">
+				method="post" style="margin: 0; text-align:right;">
 				<input type="hidden" name="page_id" value="<?php echo $page_id;?>" />
 				<input type="hidden" name="section_id" value="<?php echo $section_id;?>" />
 				<input type="hidden" name="mod_dir" value="<?php echo $mod_dir;?>" />
