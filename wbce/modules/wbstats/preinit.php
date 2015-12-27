454adda07bfd09e5dce33b42ed1ef7bf9975b908
NorHei <norbert@heimsath.org>
2015-12-27 23:57:42 +0100

    Notice in wbstats/preinit.php if no referer is set.
    Notice: Undefined index: HTTP_REFERER in S:\WBCE(phpweb)\html\modules\wbstats\preinit.php on line 6

--- wbce/modules/wbstats/preinit.php
+++ wbce/modules/wbstats/preinit.php
@@ -3,4 +3,4 @@
 //no direct file access
 if(count(get_included_files())==1) header("Location: ../index.php",TRUE,301);
 
-$referer = $_SERVER['HTTP_REFERER'];
+if (isset ($_SERVER['HTTP_REFERER'])) $referer = $_SERVER['HTTP_REFERER'];
