cb6f6a6956c490fcfb449a5eed8145c9774a90bc
NorHei <norbert@heimsath.org>
2016-01-03 09:58:14 +0100

    Moved preprocess to class wb as we needed some [wblinkxxx] in the backend.

--- wbce/framework/class.wb.php
+++ wbce/framework/class.wb.php
@@ -493,5 +493,31 @@ via the Settings panel in the backend of Website Baker
             return false;
         }
     }
+    
+    /*
+ * replace all "[wblink{page_id}]" with real links
+ * @param string &$content : reference to global $content
+ * @return void
+ * @history 100216 17:00:00 optimise errorhandling, speed, SQL-strict
+ */
+    public function preprocess(&$content)
+    {
+        global $database;
+        $replace_list = array();
+        $pattern = '/\[wblink([0-9]+)\]/isU';
+        if (preg_match_all($pattern, $content, $ids)) {
+            foreach ($ids[1] as $key => $page_id) {
+                $replace_list[$page_id] = $ids[0][$key];
+            }
+            foreach ($replace_list as $page_id => $tag) {
+                $sql = 'SELECT `link` FROM `' . TABLE_PREFIX . 'pages` WHERE `page_id` = ' . (int) $page_id;
+                $link = $database->get_one($sql);
+                if (!is_null($link)) {
+                    $link = $this->page_link($link);
+                    $content = str_replace($tag, $link, $content);
+                }
+            }
+        }
+    }
 
 }
