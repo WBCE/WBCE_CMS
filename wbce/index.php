b2bd575bc2b76e478ef2013f6bb13aac0a47060a
NorHei <norbert@heimsath.org>
2016-01-29 13:34:47 +0100

    Just a nother small cleanup for index.php
    You now schould be able to understand the whole file by just reading
    it from top to the end .
    
    Its now even possible to see the structure how WB is actual working.
    A bit strange i think :-)

--- wbce/index.php
+++ wbce/index.php
@@ -10,88 +10,104 @@
  * @license GNU GPL2 (or any later version)
  */
 
+// index file usually loaded directly or by WB accessfiles 
+// (files stored in /pages/ to represent the actual pages)
+
+// get the starttime in microseconds
 $starttime = array_sum(explode(" ", microtime()));
 
+// Set debug mode (old and deprecated new WB_DEBUG is set in config or initialize.php)
 define('DEBUG', false);
+
 // Include config file
-$config_file = dirname(__FILE__) . '/config.php';
-if (file_exists($config_file)) {
-    require_once $config_file;
+// Config file includes /framework/initialize.php as there are many 
+// scripts that wont go via the index.php we cannot change this right now.
+// Initialize loads the autoloader so from now on classes don't need to be included
+$file = dirname(__FILE__) . '/config.php';
+if (file_exists($file)) {
+    require_once $file;
 } 
 
+
 // Check if the config file has been set-up
+// If not redirect to the installer.
 if (!defined('TABLE_PREFIX')) {
-/*
- * Remark:  HTTP/1.1 requires a qualified URI incl. the scheme, name
- * of the host and absolute path as the argument of location. Some, but
- * not all clients will accept relative URIs also.
- */
-    $host = $_SERVER['HTTP_HOST'];
-    $uri = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
-    $file = 'install/index.php';
-    $target_url = 'http://' . $host . $uri . '/' . $file;
-    $sResponse = $_SERVER['SERVER_PROTOCOL'] . ' 307 Temporary Redirect';
-    header($sResponse);
-    header('Location: ' . $target_url); exit; 
+    require dirname(__FILE__) . '/framework/redirect_installer.php';
 }
 
 
 // Create new frontend object
 $wb = new frontend();
 
+
 // Figure out which page to display
 // Stop processing if intro page was shown
 $wb->page_select() or die();
+
 
 // Collect info about the currently viewed page
 // and check permissions
 $wb->get_page_details();
 
+
 // Collect general website settings
 $wb->get_website_settings();
 
+
 // OPF hook ,Load OutputFilter functions
-if(file_exists(WB_PATH .'/modules/outputfilter_dashboard/functions.php')) {
-    include(WB_PATH .'/modules/outputfilter_dashboard/functions.php');
-    // use 'cache' instead of 'nocache' to enable page-cache.
-    // Do not use 'cache' in case you use dynamic contents (e.g. snippets)!
+$file= WB_PATH .'/modules/outputfilter_dashboard/functions.php';
+if(file_exists($file)) {
+    include($file);
     opf_controller('init', 'nocache');
 }
 
+
 // Load functions available to templates, modules and code sections
 // also, set some aliases for backward compatibility
-require_once WB_PATH . '/framework/frontend.functions.php';
+require WB_PATH . '/framework/frontend.functions.php';
+
 
 //Get pagecontent in buffer for Droplets and/or Filter operations
 ob_start();
 
+
 // require template include.php 
-if (file_exists(WB_PATH . '/templates/' . TEMPLATE . '/include.php')){
-    require WB_PATH . '/templates/' . TEMPLATE . '/include.php'; 
+$file=WB_PATH . '/templates/' . TEMPLATE . '/include.php';
+if (file_exists($file)){
+    require ($file); 
 }
+
 
 //require the actual template file 
 require WB_PATH . '/templates/' . TEMPLATE . '/index.php';
+
+
+// fetch the Page content for applying filters 
 $output = ob_get_clean();
- 
+
+
 // OPF hook, apply outputfilter
 if(function_exists('opf_apply_filters')) {
     $output = opf_controller('page', $output);
 }
 
+
 // execute old frontend output filters or not
 if (!defined("WB_SUPPRESS_OLD_OPF") or !WB_SUPPRESS_OLD_OPF){
-    // Module is installed?
-    if (file_exists(WB_PATH . '/modules/output_filter/filter_routines.php')) {
-        include_once WB_PATH . '/modules/output_filter/filter_routines.php';
+    // Module is installed, filter file in place?
+    $file=WB_PATH . '/modules/output_filter/filter_routines.php';
+    if (file_exists($file)) {
+        include_once ($file);
         if (function_exists('executeFrontendOutputFilter')) {
             $output = executeFrontendOutputFilter($output);
         }
     }
 }
 
+
 // now send complete page to the browser
 echo $output;
+
 
 // end of wb-script
 exit;
