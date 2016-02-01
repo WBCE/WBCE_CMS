c7a846ca6193da92e5ecba3538ceeb40011cb7ff
NorHei <norbert@heimsath.org>
2016-02-01 23:57:32 +0100

    Custom Admintool Icons
    The template var {TOOL_ICON} now is available in admintools template.
    
    The admintool.htt from Flat Theme is adapted to this.

--- wbce/admin/admintools/index.php
+++ wbce/admin/admintools/index.php
@@ -38,21 +38,27 @@ $template->set_var('HEADING_ADMINISTRATION_TOOLS', $HEADING['ADMINISTRATION_TOOL
 $template->set_block('main_block', 'tool_list_block', 'tool_list');
 $results = $database->query("SELECT * FROM ".TABLE_PREFIX."addons WHERE type = 'module' AND `function` LIKE '%tool%' AND `directory` not in ('".(implode("','",$_SESSION['MODULE_PERMISSIONS']))."') order by name");
 if($results->numRows() > 0) {
-	while($tool = $results->fetchRow()) {
-		$template->set_var('TOOL_NAME', $tool['name']);
-		$template->set_var('TOOL_DIR', $tool['directory']);
-		// check if a module description exists for the displayed backend language
-		$tool_description = false;
-		if(function_exists('file_get_contents') && file_exists(WB_PATH.'/modules/'.$tool['directory'].'/languages/'.LANGUAGE .'.php')) {
-			// read contents of the module language file into string
-			$data = @file_get_contents(WB_PATH .'/modules/' .$tool['directory'] .'/languages/' .LANGUAGE .'.php');
-			$tool_description = get_variable_content('module_description', $data, true, false);
-		}		
-		$template->set_var('TOOL_DESCRIPTION', ($tool_description === False)? $tool['description'] :$tool_description);
-		$template->parse('tool_list', 'tool_list_block', true);
-	}
+    while($tool = $results->fetchRow()) {
+        $template->set_var('TOOL_NAME', $tool['name']);
+        $template->set_var('TOOL_DIR', $tool['directory']);
+        // check if a module description exists for the displayed backend language
+        $tool_description = false;
+        if(function_exists('file_get_contents') && file_exists(WB_PATH.'/modules/'.$tool['directory'].'/languages/'.LANGUAGE .'.php')) {
+            // read contents of the module language file into string
+            $data = @file_get_contents(WB_PATH .'/modules/' .$tool['directory'] .'/languages/' .LANGUAGE .'.php');
+            $tool_description = get_variable_content('module_description', $data, true, false);
+        }       
+        $template->set_var('TOOL_DESCRIPTION', ($tool_description === False)? $tool['description'] :$tool_description);
+        $tool_default_icon= "fa fa-graduation-cap";   
+        $tool_icon =false;
+        $data = @file_get_contents(WB_PATH .'/modules/' .$tool['directory'] .'/info.php');
+        $tool_icon = get_variable_content('module_icon', $data, true, false);
+        $template->set_var('TOOL_ICON', ($tool_icon === False)? $tool_default_icon :$tool_icon);
+        
+        $template->parse('tool_list', 'tool_list_block', true);
+    }
 } else {
-	$template->set_var('TOOL_LIST', $TEXT['NONE_FOUND']);	
+    $template->set_var('TOOL_LIST', $TEXT['NONE_FOUND']);   
 }
 
 // Parse template objects output
