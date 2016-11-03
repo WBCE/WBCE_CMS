5358376013a8515088e35c8247a01354e914e470
NorHei <norbert@heimsath.org>
2016-11-03 06:32:45 +0100

    Suppressing of Browser cache for class Insert new Constants
    WB_JS_REFRESH_BROWSER_CACHE
    If set true all JS files in class Insert get an parameter attached so
    browser thinks they are a new file.
    
    WB_CSS_REFRESH_BROWSER_CACHE
    If set true all CSS files in class insert get an parameter attached
    so browser thinks they are a new file.

--- wbce/framework/classes/class.insert.php
+++ wbce/framework/classes/class.insert.php
@@ -458,7 +458,20 @@ class Insert {
         if (empty($Content['href']) and empty($Content['style'])) return "CSS Nothing set no href no style";
    
         if (!empty($Content['title'])) $this->Css[$SetName]['title']  = $Content['title']; // not in use right now
-        if (!empty($Content['href'])) $this->Css[$SetName]['href']  = $Content['href'];
+        
+        // Href gets some special threatment so you can have an always refreshed browser cache
+        // by changing the file URL whith a get parameter
+        if (!defined('WB_CSS_REFRESH_BROWSER_CACHE') OR  WB_CSS_REFRESH_BROWSER_CACHE===true){
+            if (!empty($Content['href'])) {
+			    $sFilePath = str_replace(WB_URL, WB_PATH, $Content['href']);
+			    if(is_file($sFilePath)){				
+				    $this->Css[$SetName]['href'] = $Content['href'].'?'.filemtime ($sFilePath);
+			    }else{
+				    $this->Css[$SetName]['href'] = $Content['href'];
+			    }
+		    }
+        } 
+
         if (!empty($Content['media']))$this->Css[$SetName]['media'] = $Content['media'];
         if (!empty($Content['style']))$this->Css[$SetName]['style'] = $Content['style'];
         if (!empty($Content['setsave']))$this->Css[$SetName]['setsave'] = $Content['setsave'];
@@ -629,7 +642,18 @@ class Insert {
           //echo "\n<pre>check:\n"; print_r($Content); echo "</pre>";
         
         if (!empty($Content['position'])) $this->Js[$SetName]['position'] = $Position;
-        if (!empty($Content['src']))      $this->Js[$SetName]['src'] = $Content['src'];
+        // SRC gets some special threatment so you can have an always refreshed browser cache
+        // by changing the file URL whith a get parameter
+        if (!defined('WB_JS_REFRESH_BROWSER_CACHE') OR  WB_CSS_REFRESH_BROWSER_CACHE===true){
+            if (!empty($Content['src'])) {
+			    $sFilePath = str_replace(WB_URL, WB_PATH, $Content['src']);
+			    if(is_file($sFilePath)){				
+				    $this->Js[$SetName]['src'] = $Content['src'].'?'.filemtime ($sFilePath);
+			    }else{
+				    $this->Js[$SetName]['src'] = $Content['src'];
+			    }
+		    }
+        }   
         if (!empty($Content['script']))   $this->Js[$SetName]['script'] = $Content['script'];
         if (!empty($Content['setsave']))  $this->Js[$SetName]['setsave'] = $Content['setsave'];
 
