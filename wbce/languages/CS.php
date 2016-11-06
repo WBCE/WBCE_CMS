9210c2a9f2489659ed4438aa5090f8a993360ed6
NorHei <norbert@heimsath.org>
2016-11-06 18:53:20 +0100

    more fixes

--- wbce/languages/CS.php
+++ wbce/languages/CS.php
@@ -17,7 +17,7 @@ if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($
 	
 // Set the language information
 $language_code = 'CS';
-$language_name = 'čeština';
+$language_name = 'Čeština';
 $language_version = '2.8';
 $language_platform = '2.8.x';
 $language_author = 'WebStep, s.r.o.';
