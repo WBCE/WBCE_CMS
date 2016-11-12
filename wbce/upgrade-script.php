740d33a710a03c621396fa1ea759f1ce2345cb23
NorHei <norbert@heimsath.org>
2016-11-12 21:55:05 +0100

    Removed Admin tool for maintainance Mode, now using setting.

--- wbce/upgrade-script.php
+++ wbce/upgrade-script.php
@@ -170,6 +170,8 @@ $FAIL = ' <span class="error">FAILED</span> ';
 $DEFAULT_THEME = 'advancedThemeWbFlat';
 $stepID = 1;
 $dirRemove = array(
+'[MODULES]/maintainance_mode/'
+
 /*
 '[TEMPLATE]/allcss/',
 '[TEMPLATE]/blank/',
