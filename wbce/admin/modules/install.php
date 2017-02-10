69ae9e2a625c673bf5dc353297a600d79419cfed
NorHei <norbert@heimsath.org>
2017-02-10 07:24:44 +0100

    Installer messages caused by <?php tags in comments

--- wbce/admin/modules/install.php
+++ wbce/admin/modules/install.php
@@ -109,7 +109,7 @@ foreach($aAddonRootPath as $addon_root_path){
     // require always fetched the first cached file
     //require($temp_unzip.'info.php');
     $exec=file_get_contents ($temp_unzip.'info.php');
-    $exec=preg_replace("/^.*\<\?(php)?/uis","", $exec);
+    $exec = preg_replace("/^.<?(php)?/ui", "", trim($exec));
     eval ($exec);
 
     // Perform Add-on requirement checks before proceeding
