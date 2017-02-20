b4b426ef605fc6843ce87aa00a8803e47cbaf1f2
B. Michna <bakery@wwnw.de>
2017-02-20 01:19:47 +0100

    reworked some of the 'easier' template-files'

---
+++ wbce/templates/argos_theme_reloaded/jquery/theme.js
@@ -0,0 +1,125 @@
+/*** overall needed confirmation **************************************/
+function confirm_link(message, url) {
+	if (confirm(message)) {
+		location.href = url;
+	}
+}
+
+$(document).ready(function() {
+
+	/*** autoresize height of textareas *******************************/
+	autosize(document.querySelectorAll('textarea'));
+
+	/*** enable tabs in textareas (class "tabbed") ********************/
+	$(document).delegate('.tabbed', 'keydown', function(e) {
+		var keyCode = e.keyCode || e.which;
+
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = $(this).get(0).selectionStart;
+			var end = $(this).get(0).selectionEnd;
+
+			// set textarea value to: text before caret + tab + text after caret
+			$(this).val($(this).val().substring(0, start)
+						+ "\t"
+						+ $(this).val().substring(end));
+
+			// put caret at right position again
+			$(this).get(0).selectionStart =
+			$(this).get(0).selectionEnd = start + 1;
+		}
+	});
+
+	/*** add a click event to close inline msg ************************/
+	$('.close').click(function() {
+		$(this).parent().hide('slow');
+	});
+
+
+
+
+
+	/*** toggle the advanced links in addons **************************/
+	$('#show-advanced a').click(function(e) {
+		e.preventDefault();
+		$('#advanced-block').toggle();
+	});
+
+	/*** handle settings file-perms visibility depending on OS ***/
+	// set initial state
+	if ($('#os-linux').is(':checked') == true) {
+		$('#file-perms').show();
+	}
+	if ($('#os-windows').is(':checked') == true) {
+		$('#file-perms').hide();
+	}
+
+	// toggle state
+	$('#os-linux').click(function(e) {
+		if ($('#os-linux').is(':checked') == true) {
+			$('#file-perms').show();
+		} else {
+			$('#file-perms').hide();
+		}
+	});
+	$('#os-windows').click(function(e) {
+		if ($('#os-windows').is(':checked') == true) {
+			$('#file-perms').hide();
+		} else {
+			$('#file-perms').show();
+		}
+	});
+
+
+	/*** toggle the upload fields in media ****************************/
+	$('#unzip').click(function() {
+		if ($('#file2').css('display') == 'block') {
+			for (i=2; i<=10; i++) {
+				$('#file' + i).css('display', 'none');
+			}
+			$('#delzip').show();
+		} else {
+			for (i=2; i<=10; i++) {
+				$('#file' + i).css('display', 'block');
+			}
+			$('#delzip').hide();
+		}
+	});
+
+	/*** synchronize the upload target folder in media ****************/
+	$('#upload-target').change(function() {
+		var targetVal = $(this).val();
+		var targetRef = targetVal.substr(6,100);
+		$('#target-folder').val(targetVal);
+		browse.location.href = 'browse.php?dir=' + targetRef;
+
+	});
+
+	/*** for section-info in page -> modify ***************************/
+	$('.section-info').addClass('fg12');
+
+
+	/*** suppress the unnecessary h4 header in admintools *************/
+	/*
+	$('h4').each(function() {
+		$(this).find('a').each(function() {
+			var title = $(this).attr('title');
+			if (title == 'Admin-Tools') {
+				$(this).parent().css('display', 'none');
+			}
+		});
+	});
+	*/
+
+});
+
+
+
+
+
+
+
+
+
+
+
---
+++ wbce/templates/argos_theme_reloaded/modules/preferences/templates/preferences_form.tpl.php
@@ -0,0 +1,132 @@
+<?php if ($msgTxt != '') :?>
+    <div class="msg-box inline">
+		<?=$msgTxt?>
+		<i class="fa fa-remove close"></i>
+	</div>
+<?php endif; ?>
+
+<?php if ($errorTxt != '') :?>
+    <div class="error-box inline">
+		<?=$errorTxt?>
+		<i class="fa fa-remove close"></i>
+	</div>
+<?php endif; ?>
+
+<h2><?=$HEADING['MY_SETTINGS']?></h2>
+
+<section class="preferences fg12 content-box">
+	<div class="fg12 top bot"><?=$TOOL['DESCRIPTION']; ?></div>
+
+	<hr class="fg12">
+
+	<form name="preferences_save" id="preferences_save" action="<?=$returnUrl?>" method="post">
+		<?=$admin->getFTAN()?>
+
+		<div class="fg3">
+			<?=$TEXT['USERNAME']?>:
+		</div>
+		<div class="fg9" style="height: 30px;">
+			<?=$userName?>
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['DISPLAY_NAME']?>:
+		</div>
+		<div class="fg9">
+			<input type="text" id="display_name" name="display_name" value="<?=$userDisplayName?>">
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['LANGUAGE']?>:
+		</div>
+		<div class="fg9">
+		<select name="language" id="language" class="wdt200">
+			<?php foreach ($languageData as $l) :?>
+				<option value="<?=$l['code']?>" <?=$l['selected']?>><?=$l['name']?> (<?=$l['code']?>)</option>
+			<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['TIMEZONE']?>:
+		</div>
+		<div class="fg9">
+			<select name="timezone" id="timezone" class="wdt200">
+				<?php foreach ($timezoneData as $t) :?>
+					<option value="<?=$t['value']?>" <?=$t['selected']?>><?=$t['name']?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['DATE_FORMAT']?>:
+		</div>
+		<div class="fg9">
+			<select name="date_format" id="date_format" class="wdt200">
+				<?php foreach ($dateData as $d) :?>
+					<option value="<?=$d['value']?>" <?=$d['selected']?>><?=$d['name']?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['TIME_FORMAT']?>:
+		</div>
+		<div class="fg9">
+			<select name="time_format" id="time_format" class="wdt200">
+				<?php foreach ($timeformData as $f) :?>
+					<option value="<?=$f['value']?>" <?=$f['selected']?>><?=$f['name']?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+
+		<div class="fg3">
+			<?=$HEADING['MY_EMAIL']?>:
+		</div>
+		<div class="fg9">
+			<input type="text" id="email" name="email" value="<?=$userMail?>">
+		</div>
+
+		<div class="fg12 bot warning"><?=$TOOL['PASSWORD']?></div>
+
+		<div class="fg3">
+			<?=$TEXT['NEW_PASSWORD']?>:
+		</div>
+		<div class="fg9">
+			<input type="password" id="new_password_1" name="new_password_1" value=""><br />
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['RETYPE_NEW_PASSWORD']?>:
+		</div>
+		<div class="fg9">
+			<input type="password" id="new_password_2" name="new_password_2" value="">
+		</div>
+
+		<div class="fg3">
+			<?=$TEXT['NEED_CURRENT_PASSWORD']?>:
+		</div>
+		<div class="fg9">
+			<input type="password" id="current_password" name="current_password" value="">
+		</div>
+
+		<hr class="fg12">
+
+		<div class="fg5 push3">
+			<button type="submit" name="save_settings" class="save_settings">
+				<i class="fa fa-fw fa-save"></i>
+				<?=$TEXT['SAVE']; ?>
+			</button>
+		</div>
+		<div class="fg4 right">
+			<button type="reset" id="reset" name="reset" class="save_settings">
+				<i class="fa fa-fw fa-refresh"></i>
+				<?=$TEXT['RESET']; ?>
+			</button>
+		</div>
+	</form>
+</section>
+
+
+
+
