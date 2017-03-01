a61bfc0897c027a1160039d73b35ac6c4ad1fa1b
cwsoft <info@cwsoft.de>
2017-03-01 18:37:23 +0100

    Updated Colorbox module to v1.6.4 (fixes #180)

--- wbce/modules/colorbox/info.php
+++ wbce/modules/colorbox/info.php
@@ -3,15 +3,15 @@
 if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($z);}
 
 $module_directory     = 'colorbox';
-$module_name          = 'colorbox_v1.6';
+$module_name          = 'colorbox_v1.6.4';
 $module_function      = 'snippet';
-$module_version       = '1.6';
+$module_version       = '1.6.4';
 $module_status	      = '';
-$module_platform      = '2.8.2';
+$module_platform      = '1.x';
 $module_author        = 'Complete rewrite by Norbert Heimsath, updated by instantflorian';
 $module_license       = '<a href="http://www.gnu.org/licenses/gpl.html">GNU General Public Licencse 3.0</a>';
 $module_description   = 'ColorBox v1.6 - a full featured, light-weight, customizable lightbox based on-<a href="http://www.jacklmoore.com/colorbox">colorbox</a> For details about the License see the License file.';
 $module_home          = '';
-$module_requirements  = 'WB 2.8.2+, jQuery ';
+$module_requirements  = 'WBCE 1.x+, jQuery';
 
 
