ff8ee93727185841ba8ee7cb7535d8e31187880b
Martin Hecht <mrbaseman@gmx.de>
2017-04-19 23:54:39 +0200

    update mod_opf_sys_rel to 1.0.4

--- wbce/modules/mod_opf_sys_rel/upgrade.php
+++ wbce/modules/mod_opf_sys_rel/upgrade.php
@@ -4,16 +4,16 @@
  * Way Better Content Editing.
  * Visit http://wbce.org to learn more and to join the community.
  *
- * @copyright Ryan Djurovich (2004-2009)
- * @copyright WebsiteBaker Org. e.V. (2009-2015)
+ * @copyright       Ryan Djurovich (2004-2009)
+ * @copyright       WebsiteBaker Org. e.V. (2009-2015)
  * @copyright       WBCE Project (2015-2017)
  * @category        opffilter
  * @package         OPF Sys Rel
- * @version         1.0.3
+ * @version         1.0.4
  * @authors         Martin Hecht (mrbaseman)
  * @link            https://forum.wbce.org/viewtopic.php?id=176
  * @license         GNU GPL2 (or any later version)
- * @platform        WBCE 1.2.x 
+ * @platform        WBCE 1.2.x
  * @requirements    OutputFilter Dashboard 1.5.x and PHP 5.4 or higher
  *
  **/
@@ -34,7 +34,7 @@ if(count(get_included_files())==1) die(header("Location: ../index.php",TRUE,301)
 
 
 // function fetched from old filter routines
-// this function whith if exits, may not be at the end ... 
+// this function whith if exits, may not be at the end ...
 /**
  * function to read the current filter settings
  * @global object $database
