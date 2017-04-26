5add0a0efc6b3c6a8722ece24c39c3ef46e1dd9c
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-26 14:32:39 +0200

    Update twig
    Update twig from 1.20.0 to 1.33.3

--- wbce/modules/twig/info.php
+++ wbce/modules/twig/info.php
@@ -9,7 +9,7 @@
 $module_directory   = 'twig';
 $module_name        = 'TWIG';
 $module_function    = 'initialize';
-$module_version     = '1.0.0-rc.1';
+$module_version     = '1.33.3';
 $module_platform    = '1.2.0';
 $module_author      = 'Norbert Heimsath(heimsath.org)';
 $module_license     = 'Module: WTFPL,  Twig :<a href="http://twig.sensiolabs.org/license">http://twig.sensiolabs.org/license</a>';
