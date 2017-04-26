74e190dec5b975f1160b7c621b835f1390ae680e
Martin Hecht <mrbaseman@gmx.de>
2017-04-26 09:01:36 +0200

    update URL in header comment of outputfilter_dashboard

--- wbce/modules/outputfilter_dashboard/upload.php
+++ wbce/modules/outputfilter_dashboard/upload.php
@@ -11,7 +11,7 @@ upload.php
  * @version         1.5.4
  * @authors         Thomas "thorn" Hornik <thorn@nettest.thekk.de>, Christian M. Stefan (Stefek) <stefek@designthings.de>, Martin Hecht (mrbaseman) <mrbaseman@gmx.de>
  * @copyright       (c) 2009,2010 Thomas "thorn" Hornik, 2010 Christian M. Stefan (Stefek), 2017 Martin Hecht (mrbaseman)
- * @link            https://github.com/WebsiteBaker-modules/outpufilter_dashboard
+ * @link            https://github.com/WebsiteBaker-modules/outputfilter_dashboard
  * @link            http://forum.websitebaker.org/index.php/topic,28926.0.html
  * @link            https://forum.wbce.org/viewtopic.php?id=176
  * @link            http://addons.wbce.org/pages/addons.php?do=item&item=53
