89e065e00f425b9b4faf38be5ed9c86bac7dab4a
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-29 10:54:16 +0200

    Some updates for WBCE branding purposes
    Updated advancedThemeWbFlat language files

--- wbce/templates/advancedThemeWbFlat/languages/index.php
+++ wbce/templates/advancedThemeWbFlat/languages/index.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Website Baker Theme
- * Additional content to include to the backend
- *
- * Part off: Website Baker theme advanced_theme_wb_flat
- * More information see: info.php in main theme folder
- */
+// no directory access
+header("Location: ../index.php",TRUE,301);
 
-// prevent directory listing
-header('Location: ../../../index.php');
