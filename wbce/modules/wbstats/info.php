5381f8cc54b66a6a37ceacc9d3ea652d54ca9091
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-30 20:44:53 +0200

    Update wbstats
    Update wbstats from 1.8 to 1.9

--- wbce/modules/wbstats/info.php
+++ wbce/modules/wbstats/info.php
@@ -8,16 +8,19 @@
  * @license         http://www.gnu.org/licenses/gpl.html
  * @platform        WebsiteBaker 2.8.x
  * @requirements    PHP 5.2.2 and higher
- * @version         0.1.8
- * @lastmodified    October 22, 2014
+ * @version         0.1.9
+ * @lastmodified    Februari 20, 2015 
  *
  */
 
 $module_directory = 'wbstats';
 $module_name = 'WebsiteBaker Visitor statistics';
 $module_function = 'tool';
-$module_version = '0.1.8';
+$module_version = '0.1.9';
 $module_platform = '2.8.x';
 $module_author = 'Dev4me - Ruud Eisinga - www.dev4me.nl';
 $module_license = 'GNU General Public License';
 $module_description = 'Displays website statistics as an admintool';
+
+?>
+
