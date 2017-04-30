68d1d684977af3954b41399cd7d16fa3ab043599
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-30 20:49:41 +0200

    Update sitemap
    Update sitemap from 3.1.3 to 4.0.3

---
+++ wbce/modules/sitemap/languages/PL.php
@@ -0,0 +1,20 @@
+<?php
+// Polish Modul description
+$module_description = 'Modół do wyświetlania mapki strony (SiteMap)';
+
+// Other text
+$SMTEXT['MSETTINGS']		= 'Ustawienia podstawowe';
+#$SMTEXT['DISPLAY']			= 'Display';
+$SMTEXT['WHOLESITE']		= 'Wszystkie strony';
+$SMTEXT['CHILDREN_CP']		= 'Podstrony tej strony';
+$SMTEXT['HIDDEN_CHILDREN_CP'] = 'Schowane podstrony tej strony';
+$SMTEXT['PEERS_CP']			= 'Od tej strony w dół';
+$SMTEXT['PAGE_LEVEL_LIMIT']	= 'Ile poziomów pokazywać?';
+$SMTEXT['LSETTINGS']		= 'Ustawienia skórki (layout)';
+$SMTEXT['HEADER']			= 'Nadgłówek';
+$SMTEXT['LHEADER']			= 'Nadgłówek poziomu';
+$SMTEXT['LLOOP']			= 'Pojedyńcze strony';
+$SMTEXT['LFOOTER']			= 'Stopka poziomu';
+$SMTEXT['FOOTER']			= 'Stopka';
+$SMTEXT['MENUS']			= 'Menu';
+$SMTEXT['ALLMENUS']			= 'Wszystkie menus';
--- wbce/modules/sitemap/upgrade.php
+++ wbce/modules/sitemap/upgrade.php
@@ -18,26 +18,14 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-$dbtable = TABLE_PREFIX.'mod_sitemap';
 
 //Prevent this file from being accessed directly
-if (defined('WB_PATH') == false) {
-	exit("Cannot access this file directly");
-}
+defined('WB_PATH') or exit("Cannot access this file directly");
 
 require_once(WB_PATH.'/framework/functions.php');
 $database = new database();
 
-// Setup styles to help id errors
-echo'
-<style type="text/css">
-.good {color: green;}
-.bad  {color: red;}
-.ok   {color: blue;}
-.warn {color: yellow;}
-</style>
-';
-
+$dbtable = TABLE_PREFIX.'mod_sitemap'; 
 // Version to be installed is the same or older than currently installed version
 if ($module_version >= $new_module_version) {
 	echo '<span class="bad">';
@@ -45,34 +33,68 @@ if ($module_version >= $new_module_version) {
 	echo '</span><br />';
 	return;
 }
-// UPGRADE TO VERSION 3.1.1
-// ************************
 
+//
+// UPGRADE TO VERSION 4.0.0 or higher
+// new DB columns have been integrated
+// 
 $result = 0;
 if ($module_version < $new_module_version) {
 	// Title: Upgrading to
 	echo'<h3>SITEMAP - Upgrading to version '.$new_module_version.':</h3>';
-
-	// Get ITEMS table to see what needs to be created or modified
+	echo "<br /><b>Trying to rename database field '<i>loop</i>' to '<i>sitemaploop</i>'...</b><br />";
+	// Get ITEMS table to see what needs to be added or modified
 	$itemstable = $database->query("SELECT * FROM `".$dbtable."`");
 	$items = $itemstable->fetchRow();
+	if (!array_key_exists('sitemaploop', $items)){
+		if ($database->query("ALTER TABLE `".$dbtable."` CHANGE `loop` `sitemaploop` TEXT NOT NULL")) {
+			echo '<span class="good">Database field <i>loop</i> renamed to <i>sitemaploop</i> successfully</span><br />';
+		} else { 
+			echo '<span class="bad">'.$database->get_error().'</span><br />'; $result = 1;
+		}
+	} else { 
+		echo '<span class="ok">Database field <i>sitemaploop</i> already exists, update not needed.</span><br />';
+	}
 
-  echo "<BR><B>Trying to rename database field '<i>loop</i>' to '<i>sitemaploop</i>'...</B><BR>";
+	echo "<br /><b>Trying to add database field '<i>show_hidden</i>'...</b><br />";
 
-  if (!array_key_exists('sitemaploop', $items)){
-  		if ($database->query("ALTER TABLE `".$dbtable."` CHANGE `loop` `sitemaploop` TEXT NOT NULL")) {
-  			echo '<span class="good">Database field <i>loop</i> renamed to <i>sitemaploop</i> successfully</span><br />';
-  		} else { echo '<span class="bad">'.mysql_error().'</span><br />'; $result = 1;}
-  } else { echo '<span class="ok">Database field <i>sitemaploop</i> already exists, update not needed.</span><br />';}
+	if (!array_key_exists('show_hidden', $items)){
+		if ($database->query("ALTER TABLE `".$dbtable."` ADD `show_hidden` INT NOT NULL")) {
+			echo '<span class="good">Database field <i>show_hidden</i> added successfully</span><br />';
+		} else { 
+			echo '<span class="bad">'.$database->get_error().'</span><br />'; $result = 1;
+		}
+	} else { 
+		echo '<span class="ok">Database field <i>show_hidden</i> already exists, update not needed.</span><br />'; 
+	}
 
-  echo "<BR><B>Trying to add database field '<i>show_hidden</i>'...</B><BR>";
+	// ADD new fields for version 4.0.0
+	if (!array_key_exists('show_settings', $items)){		
+		echo "<br /><b>Trying to add database field '<i>show_settings</i>'...</b><br />";
+		echo "<br /><b>Trying to add database field '<i>menus</i>'...</b><br />";
+		echo "<br /><b>Trying to add database field '<i>layout</i>'...</b><br />";
+		// add new columns to table
+		if ($database->query(
+			"ALTER TABLE `".$dbtable."` 
+				ADD `show_settings` INT(1) NOT NULL DEFAULT '1', 
+				ADD `menus` VARCHAR(30) NOT NULL DEFAULT '0',
+				ADD `layout` varchar(128) NOT NULL DEFAULT '0'"
+		)){
+			echo '<span class="ok">DB fields <i>show_settings, menus, layout</i> added successfully.</span><br />';
+		}else{
+			echo '<span class="bad">DB fields can not be added.</span><br />';			
+		}
+	}else { 
+		echo '<span class="ok">Database fields <i>show_settings, menus, layout</i> already exist, update not needed.</span><br />'; 
+	}
 
-  if (!array_key_exists('show_hidden', $items)){
-  		if ($database->query("ALTER TABLE `".$dbtable."` ADD `show_hidden` INT NOT NULL")) {
-  			echo '<span class="good">Database field <i>show_hidden</i> added successfully</span><br />';
-  		} else { echo '<span class="bad">'.mysql_error().'</span><br />'; $result = 1;}
-  } else { echo '<span class="ok">Database field <i>show_hidden</i> already exists, update not needed.</span><br />'; }
-
-  if($result == 1) die();
 }
+// Setup styles to help id errors
 ?>
+<style type="text/css">
+.good {color: green;}
+.bad  {color: red;}
+.ok   {color: blue;}
+.warn {color: yellow;}
+</style>
+<?php 
