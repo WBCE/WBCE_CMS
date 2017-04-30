f77b2fc02ad2e1dbe1c0fa57c2db73b65d5ce4f1
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-30 15:35:15 +0200

    Update twig
    Update twig from 1.20.0 to 1.33.3

--- wbce/include/Sensio/Twig/lib/Twig/Extension/Debug.php
+++ wbce/include/Sensio/Twig/lib/Twig/Extension/Debug.php
@@ -3,18 +3,17 @@
 /*
  * This file is part of Twig.
  *
- * (c) 2011 Fabien Potencier
+ * (c) Fabien Potencier
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+/**
+ * @final
+ */
 class Twig_Extension_Debug extends Twig_Extension
 {
-    /**
-     * Returns a list of global functions to add to the existing list.
-     *
-     * @return array An array of global functions
-     */
     public function getFunctions()
     {
         // dump is safe if var_dump is overridden by xdebug
@@ -32,11 +31,6 @@ class Twig_Extension_Debug extends Twig_Extension
         );
     }
 
-    /**
-     * Returns the name of the extension.
-     *
-     * @return string The extension name
-     */
     public function getName()
     {
         return 'debug';
