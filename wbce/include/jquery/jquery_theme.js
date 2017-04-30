731cdd788f2537e2451f9534718643961d62a930
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-04-30 15:40:20 +0200

    Update jquery
    Update jquery stuff to the latest 1.x

--- wbce/include/jquery/jquery_theme.js
+++ wbce/include/jquery/jquery_theme.js
@@ -1,6 +1,6 @@
 var JQUERY_THEME = WB_URL+'/include/jquery';
 
 $(document).ready(function() {
-        $.insert(JQUERY_THEME+'/jquery-ui.css');
+        $.insert(JQUERY_THEME+'/jquery-ui.min.css');
         $.insert(JQUERY_THEME+'/jquery-ui-min.js');
 });
