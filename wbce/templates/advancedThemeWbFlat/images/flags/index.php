a9c3e13e3f9ab69e3d094196612cc90da27b2efa
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-05-13 15:16:01 +0200

    Some optimization

--- wbce/templates/advancedThemeWbFlat/images/flags/index.php
+++ wbce/templates/advancedThemeWbFlat/images/flags/index.php
@@ -1,14 +1,1 @@
-<?php
-/**
- * Website Baker Theme
- * Additional content to include to the backend
- *
- * Part off: Website Baker theme advanced_theme_wb_flat
- * More information see: info.php in main theme folder
- *
-*/
-
-// prevent directory listing
-header('Location: ../../../../index.php');
-
-?>
+<?php header("Location: ../index.php",true,301);
--- wbce/templates/advancedThemeWbFlat/index.php
+++ wbce/templates/advancedThemeWbFlat/index.php
@@ -1,14 +1,1 @@
-<?php
-/**
- * Website Baker theme: advanced_theme_wb_flat
- * This theme is an Websitebaker Theme.
- * Feel free to use it.
- *
- * More information see: info.php
- *
- */
-
-// prevent this file from being accessed directly
-if (!defined('WB_PATH')) {
-    die(header('Location: ../../index.php'));
-}
+<?php header("Location: ../index.php",true,301);
