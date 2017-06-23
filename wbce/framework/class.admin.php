02b54a638fc5e2ead7bee98b8f0c1f7926d0361a
NorHei <norbert@heimsath.org>
2017-06-23 22:48:39 +0200

    Txpofix on last change

--- wbce/framework/class.admin.php
+++ wbce/framework/class.admin.php
@@ -112,7 +112,7 @@ class admin extends wb
         // Session Timeout possibly not Defined on Upgrade
         $sSessionTimeout="7200";
         if (defined(WB_SECFORM_TIMEOUT)) $sSessionTimeout=WB_SECFORM_TIMEOUT;
-        if (defined(WB_SEESSION_TIMEOUT)) $sSessionTimeout=WB_SESSION_TIMEOUT;
+        if (defined(WB_SESSION_TIMEOUT)) $sSessionTimeout=WB_SESSION_TIMEOUT;
 
         $header_template->set_var(array(
             'WB_SESSION_TIMEOUT' => $sSessionTimeout,
