5c17e0af4e5699d28d7a5cdbd5f2cb01243454d5
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2017-07-02 18:32:31 +0200

    Fix for wbstats upgrade.php

--- wbce/modules/wbstats/upgrade.php
+++ wbce/modules/wbstats/upgrade.php
@@ -12,26 +12,10 @@
  * @lastmodified    June 29, 2017 
  *
  */
-defined('WB_PATH') OR die(header('Location: ../index.php'));
 
 
-function _db_add_field($field, $table, $desc) {
-	global $database;
-	$table = TABLE_PREFIX.$table;
-	$query = $database->query("DESCRIBE $table '$field'");
-	if(!$query || $query->numRows() == 0) { // add field
-		$query = $database->query("ALTER TABLE $table ADD $field $desc");
-		//echo (mysql_error()?mysql_error().'<br />':'');
-		$query = $database->query("DESCRIBE $table '$field'");
-		//echo (mysql_error()?mysql_error().'<br />':'');
-	}
-}
+// prevent this file from being accessed directly
+if(!defined('WB_PATH')) die(header('Location: index.php')); 
 
 $database->query("ALTER TABLE `".TABLE_PREFIX."mod_wbstats_ips` MODIFY `ip` VARCHAR(32)");
-
-
-//_db_add_field("`session`", "mod_wbstats_ips", "varchar(64) NOT NULL default '' AFTER `ip`");
-//_db_add_field("`page`", "mod_wbstats_ips", "varchar(255) NOT NULL default '' AFTER `online`");
-//_db_add_field("`loggedin`", "mod_wbstats_ips", "int(1) NOT NULL default '0' AFTER `page`");
-//_db_add_field("`bots`", "mod_wbstats_day", "int(1) NOT NULL default '0' AFTER `view`");
 
