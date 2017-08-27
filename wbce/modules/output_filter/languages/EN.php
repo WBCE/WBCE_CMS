23a90bd5dd492e9abae04f4ff0c0bdcc9da6e129
instantflorian <info@meerwinck.com>
2017-08-27 13:14:21 +0200

    Update EN.php
    small changes for displaying the same information as in german

--- wbce/modules/output_filter/languages/EN.php
+++ wbce/modules/output_filter/languages/EN.php
@@ -11,7 +11,7 @@
  */
 
 // Module description 
-$module_description = 'A tool to configure the basic output filter of WB(CE)';
+$module_description = 'A tool to configure the basic output filter of WBCE CMS';
  
 // Headings and text outputs
 $OPF['HEADING']	= 'Options: Output Filter';
@@ -19,25 +19,25 @@ $OPF['HOWTO'] = 'You can configure the output filtering with the options below.'
 $OPF['WARNING']	= '';
 
 // Text and captions of form elements
-$OPF['BASIC_CONF'] = 'Basic Email Configuration';
-$OPF['SYS_REL'] = 'Frontendoutput with  relative Urls';
-$OPF['EMAIL_FILTER'] = 'Filter Email addresses in text';
-$OPF['MAILTO_FILTER'] = 'Filter Email addresses in mailto links';
+$OPF['BASIC_CONF'] = 'Basic email configuration';
+$OPF['SYS_REL'] = 'Frontend output with  relative URLs';
+$OPF['EMAIL_FILTER'] = 'Filter email addresses in text';
+$OPF['MAILTO_FILTER'] = 'Filter email addresses in mailto links';
 $OPF['ENABLED']	= 'Enabled';
 $OPF['DISABLED'] = 'Disabled';
 
-$OPF['REPLACEMENT_CONF']= 'Email Replacements';
+$OPF['REPLACEMENT_CONF']= 'Email replacements';
 $OPF['AT_REPLACEMENT']	= 'Replace "@" by';
 $OPF['DOT_REPLACEMENT']	= 'Replace "." by';
 
-$OPF['ALL_ON_OFF'] = 'Enable/Disable all old Outputfilter';
-$OPF['DROPLETS'] = 'Droplets filter whithout this droplets won\'t function';
-$OPF['WBLINK'] = 'WB-Link Filter';
-$OPF['AUTO_PLACEHOLDER'] = 'Try to add placeholder for moving JS, CSS and more around';
-$OPF['MOVE_STUFF'] = 'CSS, JS, Meta move to placeholders if marked';
-$OPF['REPLACE_STUFF'] = 'Relplace title keywords or description if marked for that.';
-$OPF['JS_MAILTO'] = 'Use Javascript on Mailtofilter';
-$OPF['SHORT_URL'] = 'Use short url filter';
-$OPF['CSS_TO_HEAD'] = 'Use CSS to head';
+$OPF['ALL_ON_OFF'] = 'Enable/Disable all old output filter';
+$OPF['DROPLETS'] = 'Droplets filter (whithout this droplets won\'t work)';
+$OPF['WBLINK'] = 'WB-Link Filter (replace [wblinkXX] with URL of the page)';
+$OPF['AUTO_PLACEHOLDER'] = 'Add placeholder for moving JS, CSS and more around';
+$OPF['MOVE_STUFF'] = 'CSS, JS, and more will be moved to placeholders';
+$OPF['REPLACE_STUFF'] = 'Relplace title keywords or description by modules';
+$OPF['JS_MAILTO'] = 'Use Javascript on mailto-filter';
+$OPF['SHORT_URL'] = 'Use short url filter (no /pages, no .php - htaccess has to be prepared for that)';
+$OPF['CSS_TO_HEAD'] = 'Move CSS to head';
 
 
