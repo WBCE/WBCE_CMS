8277fb5af6e2fbac1710eb901599b9ed793295a7
instantflorian <info@meerwinck.com>
2017-09-18 10:37:36 +0200

    Update version.php
    I've forgotten to change alpha to beta

--- wbce/admin/interface/version.php
+++ wbce/admin/interface/version.php
@@ -11,12 +11,12 @@
  */
 
 // set WBCE version and release tag
-define('NEW_WBCE_VERSION', '1.3.0-alpha.2');
+define('NEW_WBCE_VERSION', '1.3.0-beta.1');
 if (!defined('WBCE_VERSION')) {
     define('WBCE_VERSION', NEW_WBCE_VERSION);
 }
 
-define('NEW_WBCE_TAG', '1.3.0-alpha.2');
+define('NEW_WBCE_TAG', '1.3.0-beta.1');
 if (!defined('WBCE_TAG')) {
     define('WBCE_TAG', NEW_WBCE_TAG);
 }
