335bd6c5ecff6406e884f997f643ad49a2617858
NorHei <norbert@heimsath.org>
2017-09-27 21:22:42 +0200

    Escapeable droplets
    Thanks To stefanek:
    https://forum.wbce.org/viewtopic.php?id=1633

--- wbce/modules/output_filter/filters/filterDroplets.php
+++ wbce/modules/output_filter/filters/filterDroplets.php
@@ -4,22 +4,31 @@
  * @param string $content
  * @return string 
  */
-    function doFilterDroplets($content) {
-        // check file and include
-        if(file_exists(WB_PATH .'/modules/droplets/droplets.php')) {
-            include_once(WB_PATH .'/modules/droplets/droplets.php');
-            
-            // remove <p> tags that are added by CKE editor every time you 
-            // have a droplet in an empty line
-            if(strpos($content, '<p>[[') !== false){
-                $content = str_replace('<p>[[','[[', $content);
-                $content = str_replace(']]</p>',']]', $content);
-            }  
-            
-            // load filter function 
-            if(function_exists('evalDroplets')) {
-                $content = evalDroplets($content, 'frontend');
-            }
+function doFilterDroplets($content) {
+    // check file and include
+    $sFile = WB_PATH .'/modules/droplets/droplets.php';
+    if(file_exists($sFile)) {
+        include_once $sFile;
+        
+                    
+        // remove <p> tags that are added by CKE editor every time you 
+        // have a droplet in an empty line
+  
+        if(strpos($content, '<p>[') !== false){
+            $content = str_replace('<p>[#[','[#[', $content);
+            $content = str_replace('<p>[[','[[', $content);
+            $content = str_replace(']]</p>',']]', $content);
+        } 
+
+        //remove commented droplets 
+        // example: [#[Lorem?blocks=6]] 
+        // the hash symbol will cause the droplet to be excluded from output 
+        $content = preg_replace('/\[\#\[[^]]*]]/', '', $content); 
+        
+        // load filter function 
+        if(function_exists('evalDroplets')) {
+            $content = evalDroplets($content, 'frontend');
         }
-        return $content;
     }
+    return $content;
+}
