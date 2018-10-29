11243d995189c44ccd0c1f85c6c488d34b45c787
Christoph Bleiweis <christoph.bleiweis@outlook.com>
2018-10-29 20:45:00 +0100

    Fix placeholder behavior (#351)
    * Remove empty lines after (PH/Placeholders) removal (#350)
    
    There remain ugly empty lines within the HTML output after the (PH) Placeholders are removed.
    This additional RegEx will handle it and speed up the HTML rendtionion a little bit also ;-)
    
    * Update AR header.htt - fix for Placeholder (#349)
    
    * Update Flat header.htt - fix for Placeholder (#348)

--- wbce/modules/output_filter/filters/filterRemoveSystemPh.php
+++ wbce/modules/output_filter/filters/filterRemoveSystemPh.php
@@ -1,5 +1,7 @@
 <?php 
  function doFilterRemoveSystemPh($content) {
     $content=preg_replace("/<!--\(PH\).*?-->/s" ,"", $content);
+    // remove any empty lines in the content
+    $content = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $content);
     return $content;
  }
