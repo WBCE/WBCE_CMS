3e7a33198a7fc7fed854f9aef437bd240e698fb1
WebDesignWorx <stefek@designthings.de>
2019-02-18 16:22:15 +0100

    Implement Password Strenght Checker for more convenience

--- wbce/admin/users/index.php
+++ wbce/admin/users/index.php
@@ -10,8 +10,8 @@
  * @license GNU GPL2 (or any later version)
  */
 
-require('../../config.php');
-require_once(WB_PATH.'/framework/class.admin.php');
+require '../../config.php';
+require_once WB_PATH.'/framework/class.admin.php';
 $admin = new admin('Access', 'users');
 
 $iUserStatus = 1;
@@ -20,36 +20,36 @@ unset($_GET);
 
 // Setup template object, parse vars to it, then parse it
 // Create new template object
-$template = new Template(dirname($admin->correct_theme_source('users.htt')));
-// $template->debug = true;
+$oTemplate = new Template(dirname($admin->correct_theme_source('users.htt')));
+// $oTemplate->debug = true;
 
-$template->set_file('page', 'users.htt');
-$template->set_block('page', 'main_block', 'main');
-$template->set_block("main_block", "manage_groups_block", "groups");
-$template->set_var('ADMIN_URL', ADMIN_URL);
-$template->set_var('FTAN', $admin->getFTAN());
-$template->set_var('USER_STATUS', $iUserStatus );
+$oTemplate->set_file('page', 'users.htt');
+$oTemplate->set_block('page', 'main_block', 'main');
+$oTemplate->set_block("main_block", "manage_groups_block", "groups");
 
 $UserStatusActive = 'url('.THEME_URL.'/images/user.png)';
 $UserStatusInactive = 'url('.THEME_URL.'/images/user_red.png)';
-
 $sUserTitle = ($iUserStatus == 0) ? $MENU['USERS'].' '.strtolower($TEXT['ACTIVE']) : $MENU['USERS'].' '.strtolower($TEXT['DELETED']) ;
 
-$template->set_var('TEXT_USERS', $sUserTitle.' '.$TEXT['SHOW'] );
-$template->set_var('STATUS_ICON', ( ($iUserStatus==0) ? $UserStatusActive : $UserStatusInactive) );
+$oTemplate->set_var(
+    array(
+        'ADMIN_URL'           => ADMIN_URL, 
+        'FTAN'                => $admin->getFTAN(), 
+        'USER_STATUS'         => $iUserStatus, 
+        'INPUT_NEW_PASSWORD'  => $admin->passwordField('password'), 
+        'TEXT_USERS'          => $sUserTitle.' '.$TEXT['SHOW'] , 
+        'STATUS_ICON'         => ($iUserStatus==0) ? $UserStatusActive : $UserStatusInactive, 
+        'ADVANCED_SEARCH'     => $TEXT['ADVANCED_SEARCH'], 
+        'QUICK_SEARCH_STRG_F' => $TEXT['QUICK_SEARCH_STRG_F'], 
+    )
+);
+        
+// Get existing values from database
+$sSql  = "SELECT * FROM `{TP}users` WHERE 1 AND user_id != 1 
+    AND active = ".$iUserStatus." 
+    ORDER BY `display_name`, `username`";
 
-$template->set_var('ADVANCED_SEARCH', $TEXT['ADVANCED_SEARCH'] );
-$template->set_var('QUICK_SEARCH_STRG_F', $TEXT['QUICK_SEARCH_STRG_F'] );
-
-// Get existing value from database
-$sql  = 'SELECT * FROM `'.TABLE_PREFIX.'users` ' ;
-$sql .= 'WHERE 1 ';
-$sql .= 'AND user_id != 1 ';
-$sql .=     'AND active = '.$iUserStatus.' ';
-$sql .= 'ORDER BY `display_name`,`username`';
-//echo $sql;
-
-$results = $database->query($sql);
+$results = $database->query($sSql);
 if($database->is_error()) {
     $admin->print_error($database->get_error(), 'index.php');
 }
@@ -57,130 +57,123 @@ if($database->is_error()) {
 $sUserList  = $TEXT['LIST_OPTIONS'].' ';
 $sUserList .= ($iUserStatus == 1) ? $MENU['USERS'].' '.strtolower($TEXT['ACTIVE']) : $MENU['USERS'].' '.strtolower($TEXT['DELETED']) ;
 // Insert values into the modify/remove menu
-$template->set_block('main_block', 'list_block', 'list');
-$template->set_var('USERTYPE', $sUserList);
+$oTemplate->set_block('main_block', 'list_block', 'list');
+$oTemplate->set_var('USERTYPE', $sUserList);
 if($results->numRows() > 0) {
-    // Insert first value to say please select
-    //$template->set_var('VALUE', '');
-    //$template->set_var('NAME', $sUserList);
-    //$template->set_var('STATUS', 'class="user-active"' );
-    //$template->parse('list', 'list_block', true);
     // Loop through users
     while($user = $results->fetchRow()) {
-		//print_r($user);
-		if ($user['login_when'] == 0) {
-			$lastlogin = '-';
-		} else {
-			$lastlogin = gmdate(DATE_FORMAT." ".TIME_FORMAT, $user['login_when'] + TIMEZONE);
-		}
-		if ($user['login_ip'] == 0) {
-			$lastip="0.0.0.0";
-		} else {
-			$lastip=$user['login_ip'];
-		}
-        $template->set_var('VALUE',$admin->getIDKEY($user['user_id']));
-        $template->set_var('STATUS', ($user['active']==false ? 'class="user-inactive"' : 'class="user-active"') );
-        $template->set_var('NAME', $user['display_name'].' ('.$user['username'].')&nbsp;&nbsp;&nbsp;'.$lastlogin.'&nbsp;&nbsp;&nbsp;'.$lastip);
-        $template->parse('list', 'list_block', true);
+        //print_r($user);
+        if ($user['login_when'] == 0) {
+            $lastlogin = '-';
+        } else {
+            $lastlogin = gmdate(DATE_FORMAT." ".TIME_FORMAT, $user['login_when'] + TIMEZONE);
+        }
+        if ($user['login_ip'] == 0) {
+            $lastip="0.0.0.0";
+        } else {
+            $lastip=$user['login_ip'];
+        }
+        $oTemplate->set_var('VALUE',$admin->getIDKEY($user['user_id']));
+        $oTemplate->set_var('STATUS', ($user['active']==false ? 'class="user-inactive"' : 'class="user-active"') );
+        $oTemplate->set_var('NAME', $user['display_name'].' ('.$user['username'].')&nbsp;&nbsp;&nbsp;'.$lastlogin.'&nbsp;&nbsp;&nbsp;'.$lastip);
+        $oTemplate->parse('list', 'list_block', true);
     }
 } else {
     // Insert single value to say no users were found
-    $template->set_var('NAME', $TEXT['NONE_FOUND']);
-    $template->parse('list', 'list_block', true);
+    $oTemplate->set_var('NAME', $TEXT['NONE_FOUND']);
+    $oTemplate->parse('list', 'list_block', true);
 }
 
 // Insert permissions values
 if($admin->get_permission('users_add') != true) {
-    $template->set_var('DISPLAY_ADD', 'hide');
+    $oTemplate->set_var('DISPLAY_ADD', 'hide');
 }
 if($admin->get_permission('users_modify') != true) {
-    $template->set_var('DISPLAY_MODIFY', 'hide');
+    $oTemplate->set_var('DISPLAY_MODIFY', 'hide');
 }
 if($admin->get_permission('users_delete') != true) {
-    $template->set_var('DISPLAY_DELETE', 'hide');
+    $oTemplate->set_var('DISPLAY_DELETE', 'hide');
 }
 $HeaderTitle = $HEADING['MODIFY_DELETE_USER'].' ';
 $HeaderTitle .= (($iUserStatus == 1) ? strtolower($TEXT['ACTIVE']) : strtolower($TEXT['DELETED']));
-// Insert language headings
-$template->set_var(array(
+$oTemplate->set_var(
+    array(
+        // Insert language headings
         'HEADING_MODIFY_DELETE_USER' => $HeaderTitle,
         'HEADING_ADD_USER' => $HEADING['ADD_USER'],
         'HEADING_ACCESS' => $MENU['ACCESS'],
-        'HEADING_USERS' => $MENU['USERS']
-       )
-);
-// insert urls
-$template->set_var(array(
+        'HEADING_USERS' => $MENU['USERS'],
+        // insert urls
         'ADMIN_URL' => ADMIN_URL,
         'WB_URL' => WB_URL,
-        'THEME_URL' => THEME_URL
-        )
-);
-// Insert language text and messages
-$template->set_var(array(
+        'THEME_URL' => THEME_URL,
+        // Insert language text and messages
         'TEXT_MODIFY' => $TEXT['MODIFY'],
         'TEXT_DELETE' => $TEXT['DELETE'],
         'TEXT_MANAGE_GROUPS' => ( $admin->get_permission('groups') == true ) ? $TEXT['MANAGE_GROUPS'] : "**",
         'CONFIRM_DELETE' => (($iUserStatus == 1) ? $TEXT['ARE_YOU_SURE'] : $MESSAGE['USERS_CONFIRM_DELETE'])
         )
 );
-if ( $admin->get_permission('groups') == true ) $template->parse("groups", "manage_groups_block", true);
+if ( $admin->get_permission('groups') == true ) 
+    $oTemplate->parse("groups", "manage_groups_block", true);
 // Parse template object
-$template->parse('main', 'main_block', false);
-$template->pparse('output', 'page');
+$oTemplate->parse('main', 'main_block', false);
+$oTemplate->pparse('output', 'page');
 
 // Setup template object, parse vars to it, then parse it
 // Create new template object
-$template = new Template(dirname($admin->correct_theme_source('users_form.htt')));
-// $template->debug = true;
-$template->set_file('page', 'users_form.htt');
-$template->set_block('page', 'main_block', 'main');
-$template->set_var('DISPLAY_EXTRA', 'display:none;');
-$template->set_var('ACTIVE_CHECKED', ' checked="checked"');
-$template->set_var('ACTION_URL', ADMIN_URL.'/users/add.php');
-$template->set_var('SUBMIT_TITLE', $TEXT['ADD']);
-$template->set_var('FTAN', $admin->getFTAN());
-// insert urls
-$template->set_var(array(
+$oTemplate = new Template(dirname($admin->correct_theme_source('users_form.htt')));
+// $oTemplate->debug = true;
+$oTemplate->set_file('page', 'users_form.htt');
+$oTemplate->set_block('page', 'main_block', 'main');
+
+$oTemplate->set_var(array(
+        'DISPLAY_EXTRA'      => 'display:none;', 
+        'ACTIVE_CHECKED'     => ' checked="checked"', 
+        'ACTION_URL'         => ADMIN_URL.'/users/add.php', 
+        'SUBMIT_TITLE'       => $TEXT['ADD'], 
+        'FTAN'               => $admin->getFTAN(), 
+        'INPUT_NEW_PASSWORD' => $admin->passwordField('password'), 
+        // insert urls
         'ADMIN_URL' => ADMIN_URL,
-        'WB_URL' => WB_URL,
+        'WB_URL'    => WB_URL,
         'THEME_URL' => THEME_URL
-        )
+    )
 );
 
 // Add groups to list
-$template->set_block('main_block', 'group_list_block', 'group_list');
+$oTemplate->set_block('main_block', 'group_list_block', 'group_list');
 $results = $database->query("SELECT group_id, name FROM ".TABLE_PREFIX."groups WHERE group_id != '1'");
 if($results->numRows() > 0) {
-    $template->set_var('ID', '');
-    $template->set_var('NAME', $TEXT['PLEASE_SELECT'].'...');
-    $template->set_var('SELECTED', ' selected="selected"');
-    $template->parse('group_list', 'group_list_block', true);
+    $oTemplate->set_var('ID', '');
+    $oTemplate->set_var('NAME', $TEXT['PLEASE_SELECT'].'...');
+    $oTemplate->set_var('SELECTED', ' selected="selected"');
+    $oTemplate->parse('group_list', 'group_list_block', true);
     while($group = $results->fetchRow()) {
-        $template->set_var('ID', $group['group_id']);
-        $template->set_var('NAME', $group['name']);
-        $template->set_var('SELECTED', '');
-        $template->parse('group_list', 'group_list_block', true);
+        $oTemplate->set_var('ID', $group['group_id']);
+        $oTemplate->set_var('NAME', $group['name']);
+        $oTemplate->set_var('SELECTED', '');
+        $oTemplate->parse('group_list', 'group_list_block', true);
     }
 }
 // Only allow the user to add a user to the Administrators group if they belong to it
 if(in_array(1, $admin->get_groups_id())) {
     $users_groups = $admin->get_groups_name();
-    $template->set_var('ID', '1');
-    $template->set_var('NAME', $users_groups[1]);
-    $template->set_var('SELECTED', '');
-    $template->parse('group_list', 'group_list_block', true);
+    $oTemplate->set_var('ID', '1');
+    $oTemplate->set_var('NAME', $users_groups[1]);
+    $oTemplate->set_var('SELECTED', '');
+    $oTemplate->parse('group_list', 'group_list_block', true);
 } else {
     if($results->numRows() == 0) {
-        $template->set_var('ID', '');
-        $template->set_var('NAME', $TEXT['NONE_FOUND']);
-        $template->parse('group_list', 'group_list_block', true);
+        $oTemplate->set_var('ID', '');
+        $oTemplate->set_var('NAME', $TEXT['NONE_FOUND']);
+        $oTemplate->parse('group_list', 'group_list_block', true);
     }
 }
 
 // Insert permissions values
 if($admin->get_permission('users_add') != true) {
-    $template->set_var('DISPLAY_ADD', 'hide');
+    $oTemplate->set_var('DISPLAY_ADD', 'hide');
 }
 
 // Generate username field name
@@ -197,23 +190,23 @@ while ($i <= 7) {
 
 // Work-out if home folder should be shown
 if(!HOME_FOLDERS) {
-    $template->set_var('DISPLAY_HOME_FOLDERS', 'display:none;');
+    $oTemplate->set_var('DISPLAY_HOME_FOLDERS', 'display:none;');
 }
 
 // Include the WB functions file
-require_once(WB_PATH.'/framework/functions.php');
+require_once WB_PATH.'/framework/functions.php';
 
 // Add media folders to home folder list
-$template->set_block('main_block', 'folder_list_block', 'folder_list');
+$oTemplate->set_block('main_block', 'folder_list_block', 'folder_list');
 foreach(directory_list(WB_PATH.MEDIA_DIRECTORY) AS $name) {
-    $template->set_var('NAME', str_replace(WB_PATH, '', $name));
-    $template->set_var('FOLDER', str_replace(WB_PATH.MEDIA_DIRECTORY, '', $name));
-    $template->set_var('SELECTED', ' ');
-    $template->parse('folder_list', 'folder_list_block', true);
+    $oTemplate->set_var('NAME', str_replace(WB_PATH, '', $name));
+    $oTemplate->set_var('FOLDER', str_replace(WB_PATH.MEDIA_DIRECTORY, '', $name));
+    $oTemplate->set_var('SELECTED', ' ');
+    $oTemplate->parse('folder_list', 'folder_list_block', true);
 }
 
 // Insert language text and messages
-$template->set_var(array(
+$oTemplate->set_var(array(
             'TEXT_CANCEL' => $TEXT['CANCEL'],
             'TEXT_RESET' => $TEXT['RESET'],
             'TEXT_ACTIVE' => $TEXT['ACTIVE'],
@@ -233,7 +226,7 @@ $template->set_var(array(
     );
 
 // Parse template for add user form
-$template->parse('main', 'main_block', false);
-$template->pparse('output', 'page');
+$oTemplate->parse('main', 'main_block', false);
+$oTemplate->pparse('output', 'page');
 
 $admin->print_footer();
