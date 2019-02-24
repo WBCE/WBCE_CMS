ca9977c1c4cd016a859b8a2f576ad415ca4ab65d
WebDesignWorx <stefek@designthings.de>
2019-02-24 16:55:25 +0100

    /account/ and /module/tool_account_settings Reworked files

--- wbce/modules/tool_account_settings/templates/form_login.tpl.php
+++ wbce/modules/tool_account_settings/templates/form_login.tpl.php
@@ -10,28 +10,28 @@ I::insertCssFile(get_url_from_path(__DIR__).'/forms.css', 'HEAD BTM+');
 <?php } ?>
  
 <form class="login-box" action="<?=WB_URL ?>/account/login.php" method="post" autocomplete="off">
-	<input type="hidden" name="username_fieldname" value="<?=$sUsernameField; ?>" />
-	<input type="hidden" name="password_fieldname" value="<?=$sPasswordField; ?>" />
-	<input type="hidden" name="redirect" value="<?=$oLogin->redirect_url;?>" />
-	<div class="cpForm">
-		<div class="formRow">
-			<label class="settingName" for="<?=$sUsernameField; ?>"><?=$TEXT['USERNAME']; ?></label>
-			<div class="settingValue">
-				<input type="text" id="<?=$sUsernameField; ?>" name="<?=$sUsernameField; ?>" />
-			</div>
-		</div>
-		
-		<div class="formRow">
-			<label class="settingName" for="<?=$sPasswordField; ?>"><?=$TEXT['PASSWORD']; ?></label>
-			<div class="settingValue">
-				<input type="password" id="<?=$sPasswordField; ?>" name="<?=$sPasswordField; ?>" />
-			</div>
-		</div>
-			
-		<div class="buttonsRow">
-			<input type="submit" name="submit" class="pos-right" value="<?=$TEXT['LOGIN']; ?>"  />
-		</div>
-	</div>
+    <input type="hidden" name="username_fieldname" value="<?=$sUsernameField; ?>" />
+    <input type="hidden" name="password_fieldname" value="<?=$sPasswordField; ?>" />
+    <input type="hidden" name="redirect" value="<?=$oLogin->redirect_url;?>" />
+    <div class="cpForm">
+        <div class="formRow">
+            <label class="settingName" for="<?=$sUsernameField; ?>"><?=$TEXT['USERNAME']; ?></label>
+            <div class="settingValue">
+                <input type="text" id="<?=$sUsernameField; ?>" name="<?=$sUsernameField; ?>" />
+            </div>
+        </div>
+
+        <div class="formRow">
+            <label class="settingName" for="<?=$sPasswordField; ?>"><?=$TEXT['PASSWORD']; ?></label>
+            <div class="settingValue">
+                <input type="password" id="<?=$sPasswordField; ?>" name="<?=$sPasswordField; ?>" />
+            </div>
+        </div>
+
+        <div class="buttonsRow">
+            <input type="submit" name="submit" class="pos-right" value="<?=$TEXT['LOGIN']; ?>"  />
+        </div>
+    </div>
 </form>
 <p><a href="<?=WB_URL; ?>/account/forgot.php"><?=$TEXT['FORGOTTEN_DETAILS']; ?></a></p>
 <script type="text/javascript">
