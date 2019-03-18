b754902d20a6a52b6fda213f9743094db99386b3
Colinax <christoph.bleiweis@outlook.com>
2019-03-18 23:02:47 +0100

    colorbox versioning corrected

--- wbce/modules/colorbox/info.php
+++ wbce/modules/colorbox/info.php
@@ -2,7 +2,7 @@
 /**
  * WebsiteBaker Community Edition (WBCE)
  * Way Better Content Editing.
- * Visit http://wbce.org to learn more and to join the community.
+ * Visit https://wbce.org to learn more and to join the community.
  *
  * @copyright Ryan Djurovich (2004-2009)
  * @copyright WebsiteBaker Org. e.V. (2009-2015)
@@ -13,8 +13,16 @@
 $module_directory = 'colorbox';
 $module_name = 'colorbox_v1.6.4';
 $module_function = 'snippet';
-$module_version = '1.6.4.2';
-$module_platform = '1.x';
+$module_version = '1.6.4.3';
+$module_platform = '1.0.0';
 $module_author = 'Complete rewrite by Norbert Heimsath, updated by WBCE';
 $module_license = 'GNU General Public Licencse 3.0';
 $module_description = 'ColorBox v1.6 - a full featured, light-weight, customizable lightbox based on-<a href="http://www.jacklmoore.com/colorbox">colorbox</a> For details about the License see the License file.';
+
+/**
+ * Version history
+ *
+ * 1.6.4.3
+ *        - Use own jquery instead of Google CDN in colorbox fallback
+ *
+ **/
