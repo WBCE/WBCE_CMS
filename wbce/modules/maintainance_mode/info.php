32c4d65e61f5daca97fad5a604aedcefd80af6e5
Colinax <christoph.bleiweis@outlook.com>
2019-03-18 22:44:01 +0100

    maintainance_mode versioning corrected

--- wbce/modules/maintainance_mode/info.php
+++ wbce/modules/maintainance_mode/info.php
@@ -10,9 +10,22 @@
 $module_directory = 'maintainance_mode';
 $module_name = 'Maintainance Mode Switcher';
 $module_function = 'tool';
-$module_version = '1.1.1';
-$module_platform = '1.1.0';
+$module_version = '1.1.3';
+$module_platform = '1.4.0';
 $module_author = 'Norbert Heimsath(heimsath.org)';
 $module_license	= 'WTFPL';
 $module_description = 'This tool offers a plain and simple switch to turn maintainance mode on and off.';
-$module_icon        = 'fa fa-wrench';
+$module_icon = 'fa fa-wrench';
+$module_level = 'core';
+
+/**
+ * Version history
+ *
+ * 1.1.3
+ *        - Add module_level core status
+ *        - Update module_platform 
+ *
+ * 1.1.2
+ *        - Add module_name translation
+ *
+ **/
