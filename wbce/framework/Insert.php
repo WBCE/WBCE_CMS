76c1f46cf679ff181ba694a8eb2c5d5fc43f17bb
Stefek <illuandmoore@yahoo.de>
2019-03-26 08:19:15 +0100

    Handling issue #402 (Meta Tags Are Missing #402) (#403)
    * Add Colorbox to the list of OpF Dashboard Filters
    
    * Allow Insert class to be used several times (enhancement)
    
    * Allow Insert class to be used several times (enhancement)
    
    * Handling issue #402
    (Meta Tags Are Missing #402)

--- wbce/framework/Insert.php
+++ wbce/framework/Insert.php
@@ -1258,13 +1258,15 @@ class Insert {
                 1
             );
         }
-        unset(
-            $this->_JsQueue,
-            $this->_CssQueue,
-            $this->_HtmlQueue,
-            $this->_TitleQueue,
-            $this->_MetaQueue
-        );
+
+//        unset(
+//            $this->_JsQueue,
+//            $this->_CssQueue,
+//            $this->_HtmlQueue,
+//            $this->_TitleQueue,
+//            $this->_MetaQueue
+//        );
+
         return $sContent;
     }    
     
