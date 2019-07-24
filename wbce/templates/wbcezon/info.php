b980abaf2943b14c8b75d4dade8420f3465ae9b7
instantflorian <info@meerwinck.com>
2019-07-24 10:13:29 +0200

    Small WBCEZon fixes
    Too long block name / CSS hack for anchor links

--- wbce/templates/wbcezon/info.php
+++ wbce/templates/wbcezon/info.php
@@ -2,11 +2,11 @@
 
 $template_directory = 'wbcezon';
 $template_name = 'WBCEZon';
-$template_version = '1.0';
+$template_version = '1.0.1';
 $template_platform = '1.4';
 $template_author = 'Florian Meerwinck';
 $template_license = 'MIT License';
-$template_description = 'Klassisch aufgebautes Template mit horizontaler Navigation, Content- und optionaler Seitenspalte, optionalen breiten Blöcken darüber und darunter. ';
+$template_description = 'Klassisch aufgebautes Template mit horizontaler Navigation, Content- und optionaler Seitenspalte, optionalen breiten Blöcken darüber und darunter. Die linke Spalte ist nur in der Desktop/Tablet-Ansicht sichtbar, nicht auf Smartphones. ';
 $template_license = 'Responsee is licenced under MIT, MFG Labs icon set under CC-BY 3.0 and SIL Open Font License, Open Sans is licensed under Apache license. Images from pixabay.com, Pixabay License. ';
 
 
@@ -14,7 +14,7 @@ $block[1] = 'Inhalt';
 $block[2] = 'Seitenspalte';
 $block[3] = 'Breiter Inhalt oben';
 $block[4] = 'Breiter Inhalt unten';
-$block[5] = 'Linke Spalte (nur Desktop/Tablet)';
+$block[5] = 'Linke Spalte';
 $block[99] = 'nicht zeigen';
 
 $menu[1] = 'Navigation';
@@ -25,7 +25,7 @@ if (LANGUAGE!="DE") {
 	$block[2] = 'Aside';
 	$block[3] = 'Big Content Top';
 	$block[4] = 'Big Content Bottom';
-	$block[5] = 'Left Column (Desktop/Tablet only)';
+	$block[5] = 'Left Column';
 	$block[99] = 'No Output';	
 	$menu[99] = 'Not in Nav.';
 }
