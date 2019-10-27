c2637f00496760f1572c8b7b0c625e2d7af9d207
Stefek <illuandmoore@yahoo.de>
2019-10-27 14:24:13 +0100

    handle issue with saving droplets

--- wbce/modules/droplets/save_droplet.php
+++ wbce/modules/droplets/save_droplet.php
@@ -11,7 +11,8 @@
  */
 
 require_once '../../config.php';
-require_once dirname(__FILE__) . '/functions.inc.php';
+
+$sBackURL = ADMIN_URL.'/admintools/tool.php?tool=droplets';
 
 // Include WB admin wrapper script
 $admin = new admin('admintools', 'admintools');
@@ -23,8 +24,7 @@ echo "</textarea>";
 */
 
 // check permission
-if ( $admin->get_permission('admintools') == true )
-{
+if ( $admin->get_permission('admintools') == true ) {
     // Get id
     if ( isset($_POST['droplet_id']) && is_numeric($_POST['droplet_id']) && !empty($_POST['droplet_id']))
     {
@@ -34,49 +34,41 @@ if ( $admin->get_permission('admintools') == true )
     {
         header( "Location: " . ADMIN_URL . "/pages/index.php" );
     }
-}
-else
-{
-    $admin->print_header();
-    $admin->print_error($MESSAGE['GENERIC_SECURITY_ACCESS'], ADMIN_URL.'/admintools/tool.php?tool=droplets' );
+} else {
+    $admin->print_error($MESSAGE['GENERIC_SECURITY_ACCESS'], $sBackURL );
     $admin->print_footer();
     exit();
 }
 
+$sStayURL = $sBackURL.'&do=modify&droplet_id='.$droplet_id;
 // Validate all fields
-if($admin->get_post('title') == '')
-{
-	$admin->print_error($MESSAGE['GENERIC_FILL_IN_ALL'], ADMIN_URL.'/admintools/tool.php?tool=droplets&amp;do=modify&amp;droplet_id='.$droplet_id);
-}
-else
-{
-	$title         = $admin->add_slashes($admin->get_post('title'));
-	$active        = (int) $admin->get_post('active');
-	$admin_view    = (int) $admin->get_post('admin_view');
-	$admin_edit    = (int) $admin->get_post('admin_edit');
-	$show_wysiwyg  = (int) $admin->get_post('show_wysiwyg');
-	$description   = $admin->add_slashes($admin->get_post('description'));
-	$tags          = array('<?php', '?'.'>' , '<?');
-	$content       = $admin->add_slashes(str_replace($tags, '', $_POST['savecontent']));
-	$comments      = $admin->add_slashes($admin->get_post('comments'));
-	$modified_when = time();
-	$modified_by   = (int) $admin->get_user_id(); 
+if ($admin->get_post('title') == '') {
+    $admin->print_error($MESSAGE['GENERIC_FILL_IN_ALL'], $sStayURL);
+    $admin->print_footer();
+    exit();
+    
 }
 
-// Update row
-$database->query(sprintf(
-    "UPDATE `%smod_droplets` SET `name` = '%s', active = '%s', admin_view = '%s', " .
-    "admin_edit = '%s', show_wysiwyg = '%s', description = '%s', code = '%s', " .
-    "comments = '%s', modified_when = '%s', modified_by = '%s' WHERE id = '%d'",
-    TABLE_PREFIX, $title, $active, $admin_view, $admin_edit, $show_wysiwyg,
-    $description, $content, $comments, $modified_when, $modified_by, $droplet_id
-));
+$tags = array('<?php', '?'.'>' , '<?');
+$aUpdate = array(
+    'id'            => $droplet_id,
+    'name'          => $admin->add_slashes($admin->get_post('title')),
+    'active'        => (int) $admin->get_post('active'),
+    'admin_view'    => (int) $admin->get_post('admin_view'),
+    'admin_edit'    => (int) $admin->get_post('admin_edit'),
+    'show_wysiwyg'  => (int) $admin->get_post('show_wysiwyg'),
+    'description'   => $admin->add_slashes($admin->get_post('description')),
+    'code'          => $admin->add_slashes(str_replace($tags, '', $_POST['savecontent'])),
+    'comments'      => $admin->add_slashes($admin->get_post('comments')),
+    'modified_when' => time(),
+    'modified_by'   => (int) $admin->get_user_id()
+);
+$database->updateRow('{TP}mod_droplets', 'id', $aUpdate);
 
-if($database->is_error()) {
-	$admin->print_error($database->get_error(),ADMIN_URL.'/admintools/tool.php?tool=droplets');
+if ($database->hasError()) {
+    $admin->print_error($database->getError(), $sBackURL);
 } else {
-    $admin->print_success($TEXT['SUCCESS'], ADMIN_URL.'/admintools/tool.php?tool=droplets');
+    $admin->print_success($TEXT['SUCCESS'], $sStayURL);
 }
-
 // Print admin footer
 $admin->print_footer();
