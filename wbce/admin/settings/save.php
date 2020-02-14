0df64c25a9c00d5f2ed0c4fc99471192c9e2646c
Colinax <christoph.bleiweis@outlook.com>
2020-02-14 13:46:51 +0100

    fix for wrong permissions

--- wbce/admin/settings/save.php
+++ wbce/admin/settings/save.php
@@ -89,7 +89,7 @@ if ($advanced == '') {
 } else {
     $file_mode = STRING_FILE_MODE;
     $dir_mode = STRING_DIR_MODE;
-    if ($admin->get_group_id() == '1') {
+    if ($admin->get_user_id() == '1') {
         // Work-out the octal value for file mode
         $u = 0;
         if (isset($_POST['file_u_r']) && $_POST['file_u_r'] == 'true') {
