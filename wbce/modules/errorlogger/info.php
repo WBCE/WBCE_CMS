8e06436b492d068ade206f56ff56ce2218b02da3
instantflorian <info@meerwinck.com>
2020-07-23 21:16:04 +0200

    Update Errorlogger to 1.1

--- wbce/modules/errorlogger/info.php
+++ wbce/modules/errorlogger/info.php
@@ -15,16 +15,16 @@
 // Must include code to stop this file being access directly
 if(defined('WB_PATH') == false) { die("Cannot access this file directly"); }
 
-$module_directory = 'errorlogger';
-$module_name = '  Errorlog viewer';
-$module_function = 'tool,preinit,initialize';
-$module_version = '1.0.0';
-$module_platform = '1.4.x';
-$module_author = 'Ruud Eisinga - Dev4me';
-$module_license	= 'GNU General Public License';
+$module_directory   = 'errorlogger';
+$module_name        = 'Errorlog viewer';
+$module_function    = 'tool,preinit,initialize';
+$module_version     = '1.1.0';
+$module_platform    = '1.4.x';
+$module_author      = 'Ruud Eisinga - Dev4me';
+$module_license	    = 'GNU General Public License';
 $module_description = 'Catch PHP warnings and errors into a logfile and view them using this tool.';
-$module_icon            = 'fa fa-tasks';
-//$module_level           = 'core';
+$module_icon        = 'fa fa-tasks';
+//$module_level       = 'core';
 
 
 /*
@@ -32,6 +32,15 @@ $module_icon            = 'fa fa-tasks';
  *
  * initialize.php is used to set the errorlevel to E_ALL regardless the WB setting
  *
+ */
+
+/*******************************************************************************
+	DEVELOPMENT HISTORY (Change Log):
+    
+	v.1.1.0 2020-07-23 Christian M. Stefan (Stefanek)
+            [+] Implementation of Table View
+            [c] changes to CSS file adding styles for the Table View
+            
  */
 
 
