bd4625dbdba5ba96b4bb928c1b49d6e543f6cda5
Colinax <christoph.bleiweis@outlook.com>
2021-05-07 18:54:34 +0200

    add warning message to AR

--- wbce/templates/argos_theme_reloaded/info.php
+++ wbce/templates/argos_theme_reloaded/info.php
@@ -14,7 +14,7 @@
 $template_directory = 'argos_theme_reloaded';
 $template_name = 'Argos Theme Reloaded';
 $template_function = 'theme';
-$template_version = '2.1.2';
+$template_version = '2.1.3';
 $template_platform = '1.4.0';
 $template_author = 'Bernd Michna (BerndJM / WWnW) based on the work by Jurgen Nijhuis (Argos Media) & Ruud Eisinga';
 $template_license = '<a href="http://www.wtfpl.net/">WTFPL</a>';
