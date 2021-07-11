bcda0ba866ed78e9a96ade476936b3675995054f
instantflorian <info@meerwinck.com>
2021-07-11 11:24:30 +0200

    Update NWI version file

--- wbce/modules/news_img/info.php
+++ wbce/modules/news_img/info.php
@@ -16,13 +16,16 @@
 $module_directory   = 'news_img';
 $module_name        = 'News with Images';
 $module_function    = 'page';
-$module_version     = '5.0.7';
+$module_version     = '5.0.8';
 $module_platform    = '1.4';
 $module_author      = 'Ryan Djurovich, Rob Smith, Silvia Reins, Martin Hecht, Florian Meerwinck, Bianka Martinovic';
 $module_license     = 'GNU General Public License';
 $module_description = 'This page type is designed for making a news page with Images and Lightboxeffect.';
 
 /**
+ * v5.0.8  - 2021-07-11
+ *         - Florian
+ *           * better prev/next navigation when group is selected
  *
  * v5.0.7  - 2020-09-20
  *         - Various
