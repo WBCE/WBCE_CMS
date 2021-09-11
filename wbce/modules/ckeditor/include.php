7d3e73c62913d26159ed4864eb9217e6e119dfa5
Colinax <christoph.bleiweis@outlook.com>
2021-09-11 18:11:49 +0200

    define CKEditor language

--- wbce/modules/ckeditor/include.php
+++ wbce/modules/ckeditor/include.php
@@ -18,15 +18,10 @@ if (count(get_included_files()) == 1) {
 }
 
 if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
-    // do nothing
+    /**
+     * do nothing
+     */
 } else {
-    $debug = false;
-
-    if (true === $debug) {
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-    }
-    
     /**
      * Function called by parent, default by the wysiwyg-module
      * @param string The name of the textarea to watch
@@ -38,7 +33,7 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
     {
         global $database;
         $oApp = isset($GLOBALS['admin']) ? $GLOBALS['admin'] : $GLOBALS['wb'];
-    
+
         $modAbsPath = str_replace('\\', '/', dirname(__FILE__));
         $ckeAbsPath = $modAbsPath . '/ckeditor/';
         if (isset($_SERVER['SCRIPT_FILENAME'])) {
@@ -49,20 +44,20 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
              */
             $realPath = str_replace('\\', '/', realpath('./'));
         }
-    
+
         $selfPath     = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
         $documentRoot = str_replace('\\', '/', realpath(substr($realPath, 0, strlen($realPath) - strlen($selfPath))));
         $tplAbsPath   = str_replace('\\', '/', $documentRoot . '/templates');
         $tplRelPath   = str_replace($documentRoot, '', $tplAbsPath);
         $modRelPath   = str_replace($documentRoot, '', $modAbsPath);
         $ckeRelPath   = $modRelPath . '/ckeditor/';
-    
+
         $url         = parse_url(WB_URL);
         $url['path'] = (isset($url['path']) ? $url['path'] : '');
         $ModPath     = str_replace($url['path'], '', $modRelPath);
         $ckeModPath  = str_replace($url['path'], '', $ckeRelPath);
         $tplPath     = str_replace($url['path'], '', $tplRelPath);
-    
+
         /**
          * Create new CKeditor instance.
          * But first - we've got to revamp this pretty old class a little bit.
@@ -71,9 +66,9 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
         require_once($ckeAbsPath.'ckeditor.php'); // $ckeAbsPath ends with /
         require_once($ckeAbsPath.'CKEditorPlus.php');
         $ckeditor = new CKEditorPlus($ckeRelPath);
-    
+
         $ckeditor->config['ModulVersion'] = isset($module_version) ? $module_version :  'none';
-    
+
         $temp = '';
         if (isset($oApp->page_id)) {
             $query = "SELECT `template` from `{TP}pages` where `page_id`='" . (int) $oApp->page_id . "'";
@@ -81,7 +76,17 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
         }
         $templateFolder = ($temp == "") ? DEFAULT_TEMPLATE : $temp;
         $ckeditor->setTemplatePath($templateFolder);
-    
+
+        /**
+         * Set user language
+         */
+        $ckeditor->config['language'] = strtolower(LANGUAGE);
+
+        /**
+         * The language to be used if config.language is empty and it's not possible to localize the editor to the user language.
+         */
+        $ckeditor->config['defaultLanguage'] = strtolower(DEFAULT_LANGUAGE);
+
         /**
          * Looking for the styles
          */
@@ -90,7 +95,7 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
             $ModPath.'/ckeditor/contents.css',
             'contentsCss'
         );
-    
+
         /**
          * Looking for the editor.styles at all ...
          */
@@ -100,12 +105,12 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
             'stylesSet',
             'wb:'
         );
-    
+
         /**
          * Call the filebrowser
          */
         $ckeditor->config['filebrowserBrowseUrl'] = $url['path'] . '/modules/elfinder/ef/elfinder_cke.php';
-    
+
         /**
          * Define all extra CKEditor plugins here
          */
@@ -113,22 +118,22 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
         $ckeditor->config['removePlugins']   = 'wsc,save';
         $ckeditor->config['removeButtons']   = 'Font';
         $ckeditor->config['fontawesomePath'] = WB_URL . '/include/font-awesome/css/font-awesome.min.css';
-    
+
         if ($toolbar) {
             $ckeditor->config['toolbar'] = $toolbar;
         }
-    
+
         $ckeditor->config['height'] = $height;
         $ckeditor->config['width']  = $width;
-    
+
         /**
          * Force the object to print/echo direct instead of returning the
          * HTML source string.
          */
         $ckeditor->returnOutput = false;
-    
+
         $ckeditor->reverse_htmlentities($content);
-    
+
         echo $ckeditor->to_HTML($name, $content, $ckeditor->config);
     }
 }
