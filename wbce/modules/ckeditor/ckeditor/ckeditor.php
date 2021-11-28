2faf85a067b84e23081043c2123b0a8383ad09ff
Colinax <christoph.bleiweis@outlook.com>
2021-11-28 16:18:55 +0100

    CKEditor 4.17.1

--- wbce/modules/ckeditor/ckeditor/ckeditor.php
+++ wbce/modules/ckeditor/ckeditor/ckeditor.php
@@ -32,18 +32,18 @@ class CKEditor
     /**
      * The version of %CKEditor.
      */
-    const version = '4.16.2';
+    const version = '4.17.1';
 
     /**
      * A constant string unique for each release of %CKEditor.
      */
-    const timestamp = 'L7C8';
+    const timestamp = 'LAHF';
 
     /**
      * A string indicating the creation date of %CKEditor.
      * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
      */
-    public $timestamp = "L7C8";
+    public $timestamp = "LAHF";
 
     /**
      * URL to the %CKEditor installation directory (absolute or relative to document root).
