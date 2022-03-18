f94dc2d1eae6db55ea0e5a2bbafb97700288494c
Colinax <christoph.bleiweis@outlook.com>
2022-03-18 13:59:02 +0100

    elFinder 1.1.4

--- wbce/modules/elfinder/info.php
+++ wbce/modules/elfinder/info.php
@@ -13,7 +13,7 @@
 $module_directory = 'elfinder';
 $module_name = 'elFinder';
 $module_function = 'tool';
-$module_version = '1.1.3';
+$module_version = '1.1.4';
 $module_platform = '1.5.0';
 $module_author = 'WBCE';
 $module_license = 'Modul under GPL, elFinder under BSD';
@@ -24,16 +24,17 @@ $module_level = 'core';
 /**
  * Version history
  *
+ * 1.1.4 - updated elFinder to 2.1.61
  * 1.1.3 - uses Material-Theme Gray as default
  * 1.1.2 - elFinder uses window hight
  *       - small code clean up
  * 1.1.1 - updated elFinder to 2.1.59
  * 1.1.0 - updated elFinder to 2.1.58
- * 1.0.5 - fix media directory path
- *       - WBCE 1.5.0 release version
+ * 1.0.5 - WBCE 1.5.0 release version
+ *       - fix media directory path
  * 1.0.4 - granulated permission setting according to user group configuration
  * 1.0.3 - add module_level core status
- * 1.0.2 - WBCE forum release
+ * 1.0.2 - WBCE forum beta release
  * 1.0.1 - fixed some bugs
  * 1.0.0 - initial commit
  *
