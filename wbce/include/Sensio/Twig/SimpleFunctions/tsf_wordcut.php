9c40e9c4d7cd9e0b2c1ed9df1f723db28ebbbe0c
Stefek <illuandmoore@yahoo.de>
2023-01-09 21:07:00 +0100

    Remove  $oTwig->getCompiler()->getFilename()
    This method doesn't exist anymore in Twig and causes issues when the debug_dump() function is called from Twig templates.

--- wbce/include/Sensio/Twig/SimpleFunctions/tsf_wordcut.php
+++ wbce/include/Sensio/Twig/SimpleFunctions/tsf_wordcut.php
@@ -20,6 +20,6 @@ $oTwig->addFunction(new \Twig\TwigFunction("wordcut",
 
 $oTwig->addFunction(new \Twig\TwigFunction("debug_dump", 
    function ($mVar = '', $sCaption = '', $bVarDump = false) use ($oTwig) {
-       return debug_dump($mVar, $sCaption, $bVarDump, $oTwig->getCompiler()->getFilename());
+       return debug_dump($mVar, $sCaption, $bVarDump);
    }
 ));   
