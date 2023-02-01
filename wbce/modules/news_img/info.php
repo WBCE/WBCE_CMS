2338c194132b5198770b8cb37081619c5bfee609
instantflorian <info@meerwinck.com>
2023-02-01 20:48:15 +0100

    NWI Version sync

--- wbce/modules/news_img/info.php
+++ wbce/modules/news_img/info.php
@@ -16,13 +16,17 @@
 $module_directory   = 'news_img';
 $module_name        = 'News with Images';
 $module_function    = 'page';
-$module_version     = '5.0.18';
+$module_version     = '5.0.19';
 $module_platform    = '1.4';
 $module_author      = 'Ryan Djurovich, Rob Smith, Silvia Reins, Martin Hecht, Florian Meerwinck, Bianka Martinovic';
 $module_license     = 'GNU General Public License';
 $module_description = 'This page type is designed for making a news page with Images and Lightboxeffect.';
 
 /**
+ * v5.0.19 - 2023/02/01
+ *		   - florian
+ *         ! missing changes on upgrade.php in single install package. no changes in WBCE core repo.
+ * 
  * v5.0.18 - 2023/01/27
  *         - florian
  *         ! fix issue with non-replacement of {SYSVAR:MEDIA_REL}
