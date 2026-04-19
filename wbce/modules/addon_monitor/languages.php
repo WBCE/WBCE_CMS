03565ace0a17035625c44e488644575d113677f1
Christian M. Stefan <dev@wbEasy.de>
2026-04-19 21:02:41 +0200

    Total cleanup and redesign of the `AddonMonitor` Admin-Tool. - Modules now show all their functions (scopes of operation) if they are   hybrid modules (e.g. Tool and Page or Snippet and Preinig) - Getting rid of all jQuery Plugins and changing vor simple Javascript - Restrict access to tabs (modules, templates, languages) based on user permissions - Add multilingaulity, making use of new WBCE 1.7.0 Lang class. - All queries are rewritten to work with the new WBCE 1.7.0 PDO Database class

---
+++ wbce/modules/addon_monitor/languages.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * @file       languages.php
+ * @category   admintool
+ * @package    addon_monitor
+ * @author     Christian M. Stefan (https://www.wbeasy.de)
+ * @license    http://www.gnu.org/licenses/gpl.html
+ * @platform   WBCE CMS 1.7.0
+ * 
+ * @brief      This is the sole source of translation strings for this tool.
+ *             It contains the strings for Admin-Tools overview and text keys
+ *             used in the module itself.
+ */
+
+return [
+
+    // English (EN-baseline)
+    'EN' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Monitoring of installed Add-Ons.</b> Find out what Addons (Modules, Templates, Languages) are installed and in active use.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Template Name',
+            'MODULE_NAME'      => 'Module Name',
+            'SEARCH_BY_NAME'   => 'Search by addon-name',
+            'SEARCH_BY_AUTHOR' => 'Search by author/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Author/Developer',
+            'PAGE_MODULES' => 'Page modules',
+        ]
+    ], 
+
+    // German (DE)
+    'DE' => [
+        'module_name'        => 'AddonMonitor',        
+        'module_description' => '<b>Übersicht aller Addons.</b> Zeigt welche Module, Templates und Sprachen installiert sind und ob und wo sie in Verwendung sind.',     
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Template-Name',
+            'MODULE_NAME'      => 'Modul-Name',
+            'SEARCH_BY_NAME'   => 'Nach Addon-Namen suchen',
+            'SEARCH_BY_AUTHOR' => 'Nach Autor/Dev suchen',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Autor/Developer',
+            'PAGE_MODULES' => 'Seiten-Module',
+        ]
+    ],
+
+    // Dutch (NL)
+    'NL' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Monitoring van geïnstalleerde Add-Ons.</b> Ontdek welke addons (modules, templates, talen) zijn geïnstalleerd en actief in gebruik zijn.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Template Naam',
+            'MODULE_NAME'      => 'Module Naam',
+            'SEARCH_BY_NAME'   => 'Zoeken op addon-naam',
+            'SEARCH_BY_AUTHOR' => 'Zoeken op auteur/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Auteur/Ontwikkelaar',
+            'PAGE_MODULES' => 'Pagina modules',
+        ]
+    ],
+
+    // Polish (PL)
+    'PL' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Monitorowanie zainstalowanych dodatków.</b> Sprawdź jakie addony (moduły, szablony, języki) są zainstalowane i w użyciu.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Nazwa Szablonu',
+            'MODULE_NAME'      => 'Nazwa Modułu',
+            'SEARCH_BY_NAME'   => 'Szukaj po nazwie addonu',
+            'SEARCH_BY_AUTHOR' => 'Szukaj po autorze/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Autor/Deweloper',
+            'PAGE_MODULES' => 'Moduły stron',
+        ]
+    ],
+
+    // Norwegian (NO)
+    'NO' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Overvåking av installerte tillegg.</b> Finn ut hvilke addons (moduler, maler, språk) er installert og i aktiv bruk.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Malnavn',
+            'MODULE_NAME'      => 'Modulnavn',
+            'SEARCH_BY_NAME'   => 'Søk etter addon-navn',
+            'SEARCH_BY_AUTHOR' => 'Søk etter forfatter/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Forfatter/Utvikler',
+            'PAGE_MODULES' => 'Sidemoduler',
+        ]
+    ],
+
+    // Spanish (ES)
+    'ES' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Monitoreo de Add-Ons instalados.</b> Descubre qué addons (módulos, plantillas, idiomas) están instalados y en uso activo.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Nombre de Plantilla',
+            'MODULE_NAME'      => 'Nombre de Módulo',
+            'SEARCH_BY_NAME'   => 'Buscar por nombre de addon',
+            'SEARCH_BY_AUTHOR' => 'Buscar por autor/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Autor/Desarrollador',
+            'PAGE_MODULES' => 'Módulos de página',
+        ]
+    ],
+
+    // French (FR)
+    'FR' => [
+        'module_name'        => 'AddonMonitor',
+        'module_description' => '<b>Surveillance des Add-Ons installés.</b> Découvrez quels addons (modules, templates, langues) sont installés et activement utilisés.',
+        'TXT' => [
+            'TEMPLATE_NAME'    => 'Nom du Template',
+            'MODULE_NAME'      => 'Nom du Module',
+            'SEARCH_BY_NAME'   => 'Rechercher par nom d\'addon',
+            'SEARCH_BY_AUTHOR' => 'Rechercher par auteur/dev',
+        ],
+        'TEXT' => [
+            'AUTHOR'       => 'Auteur/Développeur',
+            'PAGE_MODULES' => 'Modules de page',
+        ]
+    ],
+
+];
--- wbce/modules/addon_monitor/uninstall.php
+++ wbce/modules/addon_monitor/uninstall.php
@@ -1,6 +1,14 @@
 <?php
-if (count(get_included_files()) ==1) {
-    $z="HTTP/1.0 404 Not Found";
-    header($z);
-    die($z);
-}
+/**
+ * @file       uninstall.php
+ * @category   admintool
+ * @package    addon_monitor
+ * @author     Christian M. Stefan (https://www.wbeasy.de)
+ * @license    http://www.gnu.org/licenses/gpl.html
+ * @platform   WBCE CMS 1.7.0
+ */
+
+// Must include code to stop this file from being accessed directly
+defined('WB_PATH') or die("This file can't be accessed directly!");
+
+// no tables to uninstall
