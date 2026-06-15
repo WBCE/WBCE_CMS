1c3ba17e042d11ec87c116c4b77730ad6eb6c77d
instantflorian <info@meerwinck.com>
2026-06-15 13:07:34 +0200

    SEt default template temporarily to whiteboard
    (cherry picked from commit ac5dab91a50a72c531555582118c5106dcc38c7e)

--- wbce/install/sql/install_data.sql
+++ wbce/install/sql/install_data.sql
@@ -36,7 +36,7 @@ INSERT INTO `{TP}settings` (`setting_id`, `name`, `value`) VALUES
 (10, 'redirect_timer', '500'),
 (11, 'home_folders', 'true'),
 (12, 'warn_page_leave', '1'),
-(13, 'default_template', 'wbcezon'),
+(13, 'default_template', 'whiteboard'),
 (14, 'default_theme', 'wbce_flat_theme'),
 (15, 'default_charset', 'utf-8'),
 (16, 'multiple_menus', 'true'),
