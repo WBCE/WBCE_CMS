a790ce3e3b7d102839949ae9795a92681075fcf9
Christian M. Stefan <dev@wbEasy.de>
2026-08-10 13:06:18 +0200

    Miniform: route install_struct.sql through Database::importSql() Part of ongoing, experimental SQLite readiness work.

--- wbce/modules/miniform/info.php
+++ wbce/modules/miniform/info.php
@@ -7,40 +7,42 @@
  * @link			http://www.dev4me.nl/modules-snippets/opensource/miniform/
  * @license         http://www.gnu.org/licenses/gpl.html
  * @platform        WebsiteBaker 2.8.x / WBCE CMS 1.x
- * @requirements    PHP 5.6 and higher
- * @version         0.23.1
- * @lastmodified    July 13, 2024
+ * @requirements    PHP 8.0 and higher
+ * @version         0.24.1
+ * @lastmodified    August 8, 2026
  *
+ * v0.24.1 - multi-driver SQL corrections (MySQL/SQLite) as preparation for
+ *           experimental SQLite readiness                                      (Stefanek)
  * v0.24.0 - finish PDO migration (parameterized queries + PDO API)
- * v0.23.3 - Fix issue with AJAX message delete	(reported by Peter Reporter) (Florian)
- * v0.23.2 - Fix for the fix										  (Ruud)
- * v0.23.1 - Fix issue with AJAX message delete	(reported by young pope) (Ruud)
- * v0.23 - fix for missing captcha in linked forms					  (Ruud)
- * v0.22 - MYSQL_ASSOC -> MYSQLI_ASSOC								  (Bernd)
- * v0.21 - Move 'modify template' to separate page.                   (Stefanek)
- * v0.20 - Handles a bug with save file reported by user colinax.     (Stefanek)
- * v0.19 - Implement AdminTool functionality (WBCE CMS only).         (Stefanek)
- * v0.18 - Small correction in Ajax delete function.                  (Stefanek)
- *         Added support for Bootstrap alerts.                        (Stefanek)
- * v0.17 - Ajax integration to "delete" messages without page reload. (Stefanek)
- * v0.16 - Ajax integration to "load more" messages in the backend.   (Stefanek)
- * v0.15 - fixed bug when quotes are used in values
- *       - fixed compatibility issue with WBCE 1.4
- * v0.14 - fixed ajax file-upload issue IOS
- * v0.13 - fixed ajax caching problem for IOS
+ * v0.23.3 - Fix issue with AJAX message delete	(reported by Peter Reporter)    (Florian)
+ * v0.23.2 - Fix for the fix							(Ruud)
+ * v0.23.1 - Fix issue with AJAX message delete	(reported by young pope)        (Ruud)
+ * v0.23   - fix for missing captcha in linked forms				(Ruud)
+ * v0.22   - MYSQL_ASSOC -> MYSQLI_ASSOC                                        (Bernd)
+ * v0.21   - Move 'modify template' to separate page.                           (Stefanek)
+ * v0.20   - Handles a bug with save file reported by user colinax.             (Stefanek)
+ * v0.19   - Implement AdminTool functionality (WBCE CMS only).                 (Stefanek)
+ * v0.18   - Small correction in Ajax delete function.                          (Stefanek)
+ *           Added support for Bootstrap alerts.                                (Stefanek)
+ * v0.17   - Ajax integration to "delete" messages without page reload.         (Stefanek)
+ * v0.16   - Ajax integration to "load more" messages in the backend.           (Stefanek)
+ * v0.15   - fixed bug when quotes are used in values
+ *         - fixed compatibility issue with WBCE 1.4
+ * v0.14   - fixed ajax file-upload issue IOS
+ * v0.13   - fixed ajax caching problem for IOS
  * 	       added referer for email-template. use {REFERER}
- * v0.12 - added sender_email, session-data storing, removed autoTLS disable
- * v0.11 - added ajax handling
+ * v0.12   - added sender_email, session-data storing, removed autoTLS disable
+ * v0.11   - added ajax handling
  *
  */
 
-$module_directory = 'miniform';
-$module_name = 'MiniForm WBCE CMS Edition';
-$module_function = 'page';
-$module_version = '0.24.0';
-$module_platform = '1.4.x';
-$module_author = 'Ruud / Dev4me';
-$module_license = 'GNU General Public License';
+$module_directory   = 'miniform';
+$module_name        = 'MiniForm WBCE CMS Edition';
+$module_function    = 'page';
+$module_version     = '0.24.1';
+$module_platform    = '1.7.0';
+$module_author      = 'Ruud / Dev4me';
+$module_license     = 'GNU General Public License';
 $module_description = 'This module allows you to create a quick and simple form without complicated settings.';
 
 if(defined('WBCE_VERSION')){
--- wbce/modules/miniform/install.php
+++ wbce/modules/miniform/install.php
@@ -17,44 +17,11 @@
 if(defined('WB_PATH') == false) { exit("Cannot access this file directly"); }
 	
 // Create tables
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."mod_miniform`");
-$mod_miniform = 'CREATE TABLE IF NOT EXISTS `'.TABLE_PREFIX.'mod_miniform` ('
-	. ' `section_id` INT NOT NULL DEFAULT \'0\','
-	. ' `email` VARCHAR(128) NOT NULL DEFAULT \'\',' 
-	. ' `emailfrom` VARCHAR(128) NOT NULL DEFAULT \'\',' 
-	. ' `subject` VARCHAR(255) NOT NULL DEFAULT \'\',' 
-	. ' `confirm_user` INT NOT NULL DEFAULT \'0\',' 
-	. ' `confirm_subject` VARCHAR(255) NOT NULL DEFAULT \'\',' 
-	. ' `template` VARCHAR(64) NOT NULL DEFAULT \'form\',' 
-	. ' `successpage` INT NOT NULL DEFAULT \'0\',' 
-	. ' `use_ajax` INT NOT NULL DEFAULT \'1\',' 
-	. ' `use_recaptcha` INT NOT NULL DEFAULT \'0\',' 
-	. ' `recaptcha_key` VARCHAR(64) NOT NULL DEFAULT \'\',' 
-	. ' `recaptcha_secret` VARCHAR(64) NOT NULL DEFAULT \'\',' 
-	. ' `remote_id` VARCHAR(64) NOT NULL DEFAULT \'\',' 
-	. ' `remote_name` VARCHAR(64) NOT NULL DEFAULT \'\',' 
-	. ' `disable_tls` INT NOT NULL DEFAULT \'0\',' 
-	. ' `no_store` INT NOT NULL DEFAULT \'0\',' 
-	. ' PRIMARY KEY ( `section_id` ) '
-	. ' )'
-	. 'COLLATE=\'utf8_unicode_ci\''
-	. 'ENGINE=MyISAM';
-$database->query($mod_miniform);
-
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."mod_miniform_data`");
-$mod_miniformdata = 'CREATE TABLE IF NOT EXISTS `'.TABLE_PREFIX.'mod_miniform_data` ('
-	. ' `message_id` INT NOT NULL NOT NULL auto_increment,'
-	. ' `section_id` INT NOT NULL DEFAULT \'0\','
-	. ' `user_id` INT NOT NULL DEFAULT \'0\','
-	. ' `data` MEDIUMTEXT NOT NULL,' 
-	. ' `guid` VARCHAR(64) NOT NULL DEFAULT \'\','
-	. ' `session_data` MEDIUMTEXT NOT NULL,' 
-	. ' `submitted_when` INT NOT NULL DEFAULT \'0\',' 
-	. ' PRIMARY KEY ( `message_id` ) '
-	. ' )'
-	. 'COLLATE=\'utf8_unicode_ci\''
-	. 'ENGINE=MyISAM';
-$database->query($mod_miniformdata);
+foreach ($database->importSql(__DIR__ . '/install_struct.sql', null, false) as $r) {
+    if (!$r['ok']) {
+        echo "<h2>DB error creating " . h($r['statement']) . ": " . h($r['msg']) . "</h2>";
+    }
+}
 
 
 $path = WB_PATH.'/modules/miniform/';
@@ -73,9 +40,3 @@ foreach ( $files as $file ) {
 		}
 	}
 }
-					
-					
-
-
-
-?>
